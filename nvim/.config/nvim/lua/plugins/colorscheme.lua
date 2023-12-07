3e170c465bb8a8ddbf57d530c05c66a9e1948f22
dwaris <dwaris@itclowd.de>
2023-12-07 13:23:07 +0100

    nvim: improve theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,41 @@
 return {
-  {
-    "catppuccin/nvim",
+    'catppuccin/nvim',
     lazy = true,
-    name = "catppuccin",
-    priority = 1000,
-  },
-  {
-    "LazyVim/LazyVim",
+    name = 'catppuccin',
     opts = {
-      colorscheme = "catppuccin-mocha",
+        integrations = {
+            cmp = true,
+            dashboard = true,
+            gitsigns = true,
+            headlines = true,
+            illuminate = true,
+            indent_blankline = { enabled = true },
+            lsp_trouboule = true,
+            mason = true,
+            markdown = true,
+            mini = true,
+            native_lsp = {
+                enabled = true,
+                underlines = {
+                    errors = { 'undercurl' },
+                    hints = { 'undercurl' },
+                    warnings = { 'undercurl' },
+                    information = { 'undercurl' },
+                },
+            },
+            neotest = true,
+            neotree = true,
+            semantic_tokens = true,
+            telescope = true,
+            treesitter = true,
+            treesitter_context = true,
+            which_key = true,
+        },
     },
-  },
+    {
+        'LazyVim/LazyVim',
+        opts = {
+            colorscheme = 'catppuccin-mocha',
+        },
+    },
 }
