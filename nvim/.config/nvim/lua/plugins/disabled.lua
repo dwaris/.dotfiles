fb40f05715cbaa497aabd05d022452d96f8871da
dwaris <dwaris@daum.net>
2023-12-07 01:09:14 +0100

    vim: improve by reduce

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,7 +1,10 @@
 return {
-  {
-    "folke/noice.nvim",
-    enabled = false,
-  },
-  { "rcarriga/nvim-notify", enabled = false },
+    { 'folke/noice.nvim', enabled = false },
+    { 'rcarriga/nvim-notify', enabled = false },
+    { 'nvim-neo-tree/neo-tree.nvim', enabled = false },
+    { 'folke/flash.nvim', enabled = false },
+    { 'akinsho/bufferline.nvim', enabled = false },
+    { 'MunifTanjim/nui.nvim', enabled = false },
+    { 'stevearc/dressing.nvim', enabled = false },
+    { 'echasnovski/mini.indentscope', enabled = false },
 }
