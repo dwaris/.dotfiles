718215bc2dd274fc30042789579c61bb9bc59b5f
dwaris <dwaris@daum.net>
2023-12-07 13:23:07 +0100

    nvim: improve theme

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,18 +1,29 @@
 return {
-  {
-    "telescope.nvim",
-    opts = {
-      defaults = {
-        prompt_prefix = "> ",
-        selection_caret = ">> ",
-      },
+    {
+        'telescope.nvim',
+        opts = {
+            defaults = {
+                prompt_prefix = '> ',
+                selection_caret = '>> ',
+                border = {},
+                borderchars = {
+                    '─',
+                    '│',
+                    '─',
+                    '│',
+                    '┌',
+                    '┐',
+                    '┘',
+                    '└',
+                },
+            },
+        },
+        dependencies = {
+            'nvim-telescope/telescope-fzf-native.nvim',
+            build = 'make',
+            config = function()
+                require('telescope').load_extension 'fzf'
+            end,
+        },
     },
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
-    },
-  },
 }
