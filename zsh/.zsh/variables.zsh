793bef0617deb0cdfbb4612b3dea74b1cc413f0a
dwaris <dwaris@itclowd.de>
2023-12-30 17:02:52 +0100

    zsh: remove unused variable

--- zsh/.zsh/variables.zsh
+++ zsh/.zsh/variables.zsh
@@ -5,7 +5,6 @@ export EDITOR=nvim
 #export QT_QPA_PLATFORM=wayland
 export USE_CCACHE=1
 export CCACHE_DIR=$HOME/.ccache
-export CCACHE_TEMPDIR=$HOME/.ccache
 # f flatpak but still better than Snap lol
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_DATA_DIRS="/usr/local/share:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share"
