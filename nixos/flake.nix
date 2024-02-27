2f017894ac47dadfc72295e557f515c4837c90db
dwaris <dwaris@itclowd.de>
2024-02-27 15:40:34 +0100

    nixos: fix typo

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -10,24 +10,24 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: 
-    let 
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
 
-      nixosConfigurations = {
-        jedha = nixpkgs.lib.nixosSystem {
-          extraSpecialArgs = { inherit inputs; };
+      nixosconfigurations = {
+        jedha = nixpkgs.lib.nixossystem {
+          specialargs = { inherit inputs; };
 	  modules = [
             ./hosts/jedha/configuration.nix
             inputs.home-manager.nixosmodules.default
           ];
         };
 
-        kashyyyk = nixpkgs.lib.nixosSystem {
-          extraSpecialArgs = { inherit inputs; };
+        kashyyyk = nixpkgs.lib.nixossystem {
+          specialargs = { inherit inputs; };
 	  modules = [
             ./hosts/kashyyyk/configuration.nix
             inputs.home-manager.nixosmodules.default
