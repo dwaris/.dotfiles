facc8e5baee5e9d12d535ed913ce1dcd4212eb1e
dwaris <dwaris@itclowd.de>
2024-02-27 13:56:21 +0100

    nixos: two hosts (simple)

---
+++ nixos/hosts/jedha/home.nix
@@ -0,0 +1,95 @@
+{ config, pkgs, ... }: {
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "dwaris";
+  home.homeDirectory = "/home/dwaris";
+
+  nixpkgs.config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+  };
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    gnomeExtensions.appindicator
+    neovim
+    tmux
+    starship
+    btop
+    bottom
+    git
+    fzf
+    fd
+    ripgrep
+    gnumake
+    cmake
+    gcc
+    htop
+    rsync
+    wget
+    curl
+    stow
+    unzip
+    vlc
+    bash-completion
+    neofetch
+    rustup
+    nodejs
+    love
+    go
+    neovide
+    
+    firefox-wayland
+    vivaldi
+    vivaldi-ffmpeg-codecs
+    
+    gnome.gnome-tweaks
+    discord
+    nextcloud-client
+    alacritty
+    rnote
+    joplin-desktop
+    xournalpp
+    vscode-fhs
+    texlive.combined.scheme-medium
+    ffmpeg
+
+    microsoft-edge
+    floorp
+  ];
+
+  
+#  home.file = {
+#    ".config/alacritty" = {
+#      source = "${dotfiles}/alacritty/.config/alacritty";
+#      recursive = true;
+#    };
+#    ".bashrc".source = "${dotfiles}/bash/.bashrc";
+#    ".inputrc".source = "${dotfiles}/bash/.inputrc";
+#    ".config/starship/starship.toml".source = "${dotfiles}/bash/.config/starship.toml";
+#    ".config/nvim" = {
+#      source = "${dotfiles}/nvim/.config/nvim";
+#      recursive = true;
+#    };
+#    ".tmux.conf".source = "${dotfiles}/tmux/.tmux.conf";
+#    ".vimrc".source = "${dotfiles}/vim/.vimrc";
+#    ".ssh/config".source = "${dotfiles-private}/ssh/.ssh/config";
+#    ".gitconfig".source = "${dotfiles-private}/git/.gitconfig";
+#  };
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    BROWSER = "floorp";
+    TERMINAL = "alacritty";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+}
