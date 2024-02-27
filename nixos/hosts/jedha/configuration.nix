02db45db6a4357791dd3003851fb57ea6979ea58
dwaris <dwaris@daum.net>
2024-02-27 13:56:21 +0100

    nixos: two hosts (simple)

---
+++ nixos/hosts/jedha/configuration.nix
@@ -0,0 +1,141 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+	inputs.home-manager.nixosModules.default
+    ];
+
+  boot.loader.grub = {
+    enable = true;
+    zfsSupport = true;
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+    useOSProber = true;
+    mirroredBoots = [
+      { devices = [ "nodev"]; path = "/boot"; }
+    ];
+  };
+
+  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  networking.hostName = "jedha"; # Define your hostname.
+  
+  networking.hostId = "74f65184";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Enable the XWayland Fallback windowing system.
+  programs.xwayland.enable = true;
+
+  # Configure keymap in and Dispay Manager
+  services.xserver = {
+    xkb = {
+      layout = "eu";
+    };
+    enable = true;
+    displayManager = {
+      gdm = {
+        enable = true;
+        wayland = true;
+      };
+    };
+
+    desktopManager = {
+      gnome = {
+        enable = true;
+      };
+    };
+    videoDrivers = [ "amdgpu" ];
+  };
+
+  # Configure console keymap
+  console.keyMap = "us";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = false;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.dwaris = {
+    isNormalUser = true;
+    description = "dwaris";
+    shell = pkgs.bash;
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+  # Enable Flakes
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  home-manager = {
+    extraSpecialArgs = {inherit inputs;};
+    users = {
+      "dwaris" = import ./home.nix;
+    };
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    wl-clipboard
+  ];
+
+  fonts.packages = with pkgs; [
+    noto-fonts
+    source-code-pro
+  ];
+
+  # Automatic Garbage Collection
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+  
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  services.zfs.autoSnapshot.enable = true;
+  services.zfs.autoScrub.enable = true;  
+
+  security.pki.certificates = [ "/etc/ssl/certs/root_ca.crt" ];
+  
+  services.flatpak.enable = true;
+  
+  system.stateVersion = "23.11"; # Did you read the comment?
+}
