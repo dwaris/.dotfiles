8e2eee3c647e90ea0d5b844ba318cf5cd60dd5e8
dwaris <dwaris@itclowd.de>
2024-02-28 01:12:04 +0100

    use .ogg extension for compatibility reasons

--- scripts/.scripts/opus_enc.sh
+++ scripts/.scripts/opus_enc.sh
@@ -2,7 +2,7 @@
 
 mkdir final
 
-find . -name "*.flac" | parallel 'opusenc {} --bitrate 192 --music ./final/{.}.opus'
-mv final/*.opus .
+find . -name "*.flac" | parallel 'opusenc {} --bitrate 192 --music ./final/{.}.ogg'
+mv final/*.ogg .
 rm ./*.flac
 rmdir final
