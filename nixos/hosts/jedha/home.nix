7126f09c289a6dde7a7a640b10bbb80796f95bc1
dwaris <dwaris@itclowd.de>
2024-03-30 01:13:12 +0100

    nixos: fixup staging

--- nixos/hosts/jedha/home.nix
+++ nixos/hosts/jedha/home.nix
@@ -32,7 +32,6 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-  programs.virt-manager.enable = true;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
