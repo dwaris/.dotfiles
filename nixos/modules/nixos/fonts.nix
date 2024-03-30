5c536d360c7a69ebf8dc855ac9c38d3d06ffba9d
dwaris <dwaris@itclowd.de>
2024-03-30 10:20:12 +0100

    nixos: minor cleanups

---
+++ nixos/modules/nixos/fonts.nix
@@ -0,0 +1,11 @@
+{ config, lib, pkgs, ...}: {
+    fonts.packages = with pkgs; [
+        noto-fonts
+        noto-fonts-cjk
+        noto-fonts-emoji
+        liberation_ttf
+        fira-code
+        fira-code-symbols
+        (nerdfonts.override { fonts = [ "SourceCodePro" "FiraCode" ]; })
+    ];
+}
