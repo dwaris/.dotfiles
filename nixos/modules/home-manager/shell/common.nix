35478e26adf319aa3a57e412da7bf18eab6bc4e1
dwaris <dwaris@daum.net>
2024-04-14 22:34:06 +0200

    nixos: migrate shell to nix

--- nixos/modules/home-manager/shell/common.nix
+++ nixos/modules/home-manager/shell/common.nix
@@ -1,8 +1,29 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, ... }: {
     home.packages = with pkgs; [
         stow
-        starship
-        fzf
-        bash-completion
     ];
+    programs.bash.enable = true;
+    programs.fzf.enable = true;
+    programs.starship.enable = true;
+
+    programs.starship.settings = {
+        scan_timeout = 10;
+        character = {
+            success_symbol = ''[\$](bold green)'';
+            error_symbol = ''[\$](bold red)'';
+        };
+        username = {
+            style_user = "bold purple";
+            style_root = "bold red";
+            format = "[$user]($style)";
+            disabled = false;
+            show_always = true;
+        };
+        hostname = {
+            ssh_only = false;
+            format = "[@](bolde)[$hostname](bold yellow) ";
+            disabled = false;
+        };
+        cmd_duration.disabled = true;
+    };
 }
