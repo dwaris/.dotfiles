570d565088d60f97b1e88cb92615b073296b10d5
dwaris <malina@rhrk.uni-kl.de>
2024-04-21 18:46:29 +0200

    nixos: test tiling wms

--- nixos/modules/hypr.nix
+++ nixos/modules/hypr.nix
@@ -11,28 +11,18 @@
         };
     };
 
-    programs.sway = {
-      enable = true;
-      wrapperFeatures.gtk = true; # so that gtk works properly
-      extraPackages = with pkgs; [
-        swaylock
-        swayidle
-        wl-clipboard
-        wf-recorder
-        mako # notification daemon
-        grim
-      #kanshi
-        slurp
-        alacritty # Alacritty is the default terminal in the config
-        wofi
-      ];
-      extraSessionCommands = ''
-        export SDL_VIDEODRIVER=wayland
-        export QT_QPA_PLATFORM=wayland
-        export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-        export _JAVA_AWT_WM_NONREPARENTING=1
-        export MOZ_ENABLE_WAYLAND=1
-      '';
+    programs.hyprland = {
+        # Install the packages from nixpkgs
+        enable = true;
+        # Whether to enable XWayland
+        extraPackages = with pkgs; [
+            
+            wl-clipboard
+            wf-recorder
+            alacritty # Alacritty is the default terminal in the config
+            wofi
+        ];
+        xwayland.enable = true;
     };
 
     programs.waybar.enable = true;
