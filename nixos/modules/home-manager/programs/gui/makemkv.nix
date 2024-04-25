6da3f59f241eea8204feb738846bd09a75baea7c
dwaris <dwaris@itclowd.de>
2024-04-25 14:31:11 +0200

    nixos: fix makemkv

--- nixos/modules/home-manager/programs/gui/makemkv.nix
+++ nixos/modules/home-manager/programs/gui/makemkv.nix
@@ -1,18 +1,5 @@
-{  pkgs, lib,  config, ...}:
-let
-  cfg = config.programs.makemkv;
-in
-{
-  options = {
-    programs.makemkv.enable = lib.mkEnableOption "MakeMKV";
-  };
-
-  config = lib.mkIf cfg.enable {
-    home-manager.users.dwaris = {
-      home.packages = with pkgs; [
-        makemkv
-      ];
-    };
-    boot.kernelModules = [ "sg" ];
-  };
+{  pkgs, lib, ...}: {
+  home.packages = with pkgs; [
+      makemkv
+  ];
 }
