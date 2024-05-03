7950b73e27c3e0ed6399c7088487847e6b62f1a6
dwaris <dwaris@itclowd.de>
2024-05-03 22:10:07 +0200

    add wsl support

---
+++ nixos/hosts/wsl/configuration.nix
@@ -0,0 +1,26 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, ... }: {
+    imports =
+        [
+            ../../modules/system.nix
+            # Include the results of the hardware scan.
+            ./hardware-configuration.nix
+        ];
+
+    wsl.enable = true;
+    wsl.defaultUser = "dwaris";
+
+    boot.tmp.cleanOnBoot = true;
+
+    networking.hostName = "wsl"; # Define your hostname.
+    networking.hostId = "533cdfa7";
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [ ];
+
+    system.stateVersion = "23.05"; # Did you read the comment?
+}
