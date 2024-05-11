de60103560d48213d453e6b3efdf46ef389655d2
dwaris <malina@rhrk.uni-kl.de>
2024-05-11 13:08:50 +0200

    nixos: enable ssh wallet

--- nixos/modules/system.nix
+++ nixos/modules/system.nix
@@ -41,10 +41,6 @@
 
     i18n = {
         defaultLocale = "en_US.UTF-8";
-        supportedLocales = [
-            "en_US.UTF-8/UTF-8"
-            "de_DE.UTF-8/UTF-8"
-        ];
         extraLocaleSettings = {
             LC_ADDRESS = "de_DE.UTF-8";
             LC_IDENTIFICATION = "de_DE.UTF-8";
