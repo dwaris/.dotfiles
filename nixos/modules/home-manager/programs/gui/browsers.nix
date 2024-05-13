88817136d52b2968e29f310f59232db1ed727cda
dwaris <dwaris@itclowd.de>
2024-05-13 11:15:14 +0200

    nixos: chromium: fix widevine

--- nixos/modules/home-manager/programs/gui/browsers.nix
+++ nixos/modules/home-manager/programs/gui/browsers.nix
@@ -2,11 +2,13 @@
     programs = {
     chromium = {
         enable = true;
+        package = (pkgs.chromium.override { enableWideVine = true; });
         extensions = [
             "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
             "nngceckbapebfimnlniiiahkandclblb" # bitwarden
             "lanfdkkpgfjfdikkncbnojekcppdebfp" # canvas fingerprint defender
             "mdlbikciddolbenfkgggdegphnhmnfcg" # netflix 1080p new?
+            "ponfpcnoihfmfllpaingbgckeeldkhle" # block yt-shorts
             "efobhjmgoddhfdhaflheioeagkcknoji" # vertical tabs
             "ldgfbffkinooeloadekpmfoklnobpien" # raindrop.io
             "bkkmolkhemgaeaeggcmfbghljjjoofoh" # catppuccin mocha theme
