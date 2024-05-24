97a964d3759c88dda0441fffc5de1cf958d850fd
dwaris <dwaris@itclowd.de>
2024-05-24 21:33:22 +0200

    nixos: remove standard discord

--- nixos/modules/home-manager/programs/gui/messenger.nix
+++ nixos/modules/home-manager/programs/gui/messenger.nix
@@ -2,11 +2,6 @@
     home.packages = with pkgs; [
         element-desktop
         telegram-desktop
-
-        (pkgs.discord.override {
-            withOpenASAR = true;
-            withVencord = true;
-        })
         vesktop
     ];
 }
