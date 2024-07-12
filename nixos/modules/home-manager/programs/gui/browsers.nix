a552d491bc7c254d5add81de172a8ef8d4fb8483
dwaris <malina@rhrk.uni-kl.de>
2024-07-12 17:36:00 +0200

    nixos: use firefox and not floorp

--- nixos/modules/home-manager/programs/gui/browsers.nix
+++ nixos/modules/home-manager/programs/gui/browsers.nix
@@ -14,11 +14,39 @@
             "bkkmolkhemgaeaeggcmfbghljjjoofoh" # catppuccin mocha theme
         ];
     };
+    firefox = {
+        enable = true;
+        profiles = {
+            dwaris = {
+                id = 0; # The profile ID, 0 is the default profile
+                name = "dwaris";
+            };
+        };
+        policies = {
+            AutofillAddressEnabled = false;
+            AutofillCreditCardEnabled = false;
+            Certificates = {
+                Install = ["/etc/ssl/certs/root_ca.crt"];
+            };
 
+            CaptivePortal = false;
+            DisableFirefoxStudies = true;
+            DisablePocket = true;
+            DisableTelemetry = true;
+            FirefoxHome = {
+                Pocket = false;
+                Snippets = false;
+            };
+            UserMessaging = {
+                ExtensionRecommendations = false;
+                SkipOnboarding = true;
+            };
+        };
+    };
   };
 
     home.packages = with pkgs; [
-        floorp
+        #floorp
         tor-browser
         #(vivaldi.override {
         #    proprietaryCodecs = true;
