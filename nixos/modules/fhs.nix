3041694ba8a5d6d22cf9e01f0c7af47e9abef018
dwaris <dwaris@itclowd.de>
2024-09-15 23:12:15 +0200

    nixos: fhs cleanup

--- nixos/modules/fhs.nix
+++ nixos/modules/fhs.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   # ......omit many configurations
@@ -7,25 +12,34 @@
     # ......omit many packages
 
     # Create an FHS environment using the command `fhs`, enabling the execution of non-NixOS packages in NixOS!
-    (let base = pkgs.appimageTools.defaultFhsEnvArgs; in
-      pkgs.buildFHSUserEnv (base // {
-      name = "fhs";
-      targetPkgs = pkgs: (
-        # pkgs.buildFHSUserEnv provides only a minimal FHS environment,
-        # lacking many basic packages needed by most software.
-        # Therefore, we need to add them manually.
-        #
-        # pkgs.appimageTools provides basic packages required by most software.
-        (base.targetPkgs pkgs) ++ with pkgs; [
-          pkg-config
-          ncurses
-          # Feel free to add more packages here if needed.
-        ]
-      );
-      profile = "export FHS=1";
-      runScript = "bash";
-      extraOutputsToInstall = ["dev"];
-    }))
+    (
+      let
+        base = pkgs.appimageTools.defaultFhsEnvArgs;
+      in
+      pkgs.buildFHSUserEnv (
+        base
+        // {
+          name = "fhs";
+          targetPkgs =
+            pkgs:
+            (
+              # pkgs.buildFHSUserEnv provides only a minimal FHS environment,
+              # lacking many basic packages needed by most software.
+              # Therefore, we need to add them manually.
+              #
+              # pkgs.appimageTools provides basic packages required by most software.
+              (base.targetPkgs pkgs)
+              ++ (with pkgs; [
+                pkg-config
+                ncurses
+              ])
+            );
+          profile = "export FHS=1";
+          runScript = "bash";
+          extraOutputsToInstall = [ "dev" ];
+        }
+      )
+    )
   ];
 
   # ......omit many configurations
