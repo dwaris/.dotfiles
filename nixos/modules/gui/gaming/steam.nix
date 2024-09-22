7dd0240e0d437cea80b51d11240bf998702097ac
dwaris <malina@rhrk.uni-kl.de>
2024-09-23 00:18:10 +0200

    nixos: steam cleanup

--- nixos/modules/gui/gaming/steam.nix
+++ nixos/modules/gui/gaming/steam.nix
@@ -8,19 +8,16 @@
 {
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+
+    remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
   };
-  nixpkgs.config.allowUnfreePredicate =
-    pkg:
-    builtins.elem (lib.getName pkg) [
-      "steam"
-      "steam-original"
-      "steam-run"
-    ];
+
   boot.kernel.sysctl = {
     "vm.max_map_count" = 16777216;
     "fs.file-max" = 524288;
   };
+
   programs.gamemode.enable = true;
+  programs.gamescope.enable = true;
 }
