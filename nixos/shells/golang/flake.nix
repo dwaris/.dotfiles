287c1e31fd2ce56ba9737109341b67f8a545245e
dwaris <malina@rhrk.uni-kl.de>
2024-09-29 18:17:43 +0200

    nixos: add golang dev shell

---
+++ nixos/shells/golang/flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              go
+              gotools
+              gopls
+              golangci-lint
+            ];
+          };
+      }
+    );
+}
