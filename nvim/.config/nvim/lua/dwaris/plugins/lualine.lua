f2cfb05485abc88f6a994311e14acf78dfa2129c
dwaris <malina@rhrk.uni-kl.de>
2024-10-06 15:22:29 +0200

    nixos: remove deprecated/unused code

--- nvim/.config/nvim/lua/dwaris/plugins/lualine.lua
+++ nvim/.config/nvim/lua/dwaris/plugins/lualine.lua
@@ -109,13 +109,14 @@ ins_left {
 
 ins_right {
     function()
-        local msg = ''
-        local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-        local clients = vim.lsp.get_active_clients()
+        local msg = 'No Active Lsp'
+        local buf_ft = vim.api.nvim_get_option_value("filetype", {})
+        local clients = vim.lsp.get_clients()
         if next(clients) == nil then
             return msg
         end
         for _, client in ipairs(clients) do
+            ---@diagnostic disable-next-line: undefined-field
             local filetypes = client.config.filetypes
             if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
                 return client.name
