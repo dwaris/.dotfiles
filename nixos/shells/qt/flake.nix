cc6f544b235fbef35b6acb8b9ee13a697e32090a
dwaris <dwaris@itclowd.de>
2024-10-06 10:11:00 +0200

    nixos: prepare qt/opengl shell

---
+++ nixos/shells/qt/flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              qt5.full
+              clang
+              clang-tools
+              cmake
+            ];
+          };
+      }
+    );
+}
