394c082abeb5471e7130099d06385911b48005c2
dwaris <dwaris@itclowd.de>
2024-12-08 23:04:18 +0100

    nixos: track latest nix-flatpak

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -10,7 +10,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.4.1";
+    nix-flatpak.url = "github:gmodena/nix-flatpak/";
 
     lanzaboote = {
       url = "github:nix-community/lanzaboote";
