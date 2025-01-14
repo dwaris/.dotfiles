45c1445e61e0396a1c001999ef6c0e9457a87853
dwaris <malina@rhrk.uni-kl.de>
2025-01-14 17:16:19 +0100

    nixos: hypr: setup complete

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -7,7 +7,12 @@
 
 {
 
-  programs.hyprland.enable = true;
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    xwayland.enable = true;
+  };
+
   services.xserver = {
     enable = true;
     xkb = {
@@ -21,13 +26,61 @@
     };
   };
 
+  xdg.autostart.enable = true;
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+  };
+
+  security = {
+    pam.services.gdm-password.enableGnomeKeyring = true;
+  };
+  environment.variables.XDG_RUNTIME_DIR = "/run/user/$UID";
+  programs.ssh.startAgent = true;
+
   environment.systemPackages = with pkgs; [
     hyprpaper
-    rofi-wayland
+    hyprpolkitagent
+    hyprlock
+
     waybar
-    kitty
-    gnome-icon-theme
+    pavucontrol
+    dunst
+    playerctl
+    networkmanagerapplet
+    rofi-wayland
+
+    adwaita-icon-theme
+    nautilus
+    wl-clipboard
+    libsecret
   ];
 
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
+  };
+
+  services = {
+    gvfs.enable = true;
+    devmon.enable = true;
+    udisks2.enable = true;
+    upower.enable = true;
+    power-profiles-daemon.enable = true;
+    accounts-daemon.enable = true;
+    gnome = {
+      evolution-data-server.enable = true;
+      glib-networking.enable = true;
+      gnome-keyring.enable = true;
+      gnome-online-accounts.enable = true;
+    };
+  };
+
+  #  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
