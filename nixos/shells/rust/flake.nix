cbcf472970e226121f686a5fd21f509d4f1e3e7d
dwaris <dwaris@itclowd.de>
2025-03-15 18:13:02 +0100

    nixos: shell: rust: build crates that require external system libraries

--- nixos/shells/rust/flake.nix
+++ nixos/shells/rust/flake.nix
@@ -22,6 +22,10 @@
             buildInputs = [
               rust-analyzer
               rustup
+              
+              openssl
+              pkg-config
+
             ];
           };
       }
