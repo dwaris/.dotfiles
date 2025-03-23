dfa12020fd3fe859791f368f2da8ca139f1e4e35
dwaris <malina@rhrk.uni-kl.de>
2025-03-23 17:44:38 +0100

    nvim: refactor configuration and keymaps, separate settings into distinct files

--- nvim/.config/nvim/lua/dwaris/init.lua
+++ nvim/.config/nvim/lua/dwaris/init.lua
@@ -1,35 +1,3 @@
-require 'dwaris.lazy'
+require 'dwaris.set'
 require 'dwaris.keymaps'
-
-local opt = vim.opt
-
-opt.expandtab = true
-opt.hlsearch = true
-opt.number = true
-opt.mouse = 'a'
-opt.clipboard = 'unnamedplus'
-opt.breakindent = true
-opt.swapfile = false
-opt.backup = false
-opt.undodir = os.getenv 'HOME' .. '/.nvim/undodir'
-opt.undofile = true
-
-opt.ignorecase = true
-opt.smartcase = true
-opt.scrolloff = 8
-opt.signcolumn = 'yes'
-opt.updatetime = 250
-opt.timeoutlen = 300
-opt.completeopt = 'menuone,noselect'
-opt.termguicolors = true
-
-opt.list = true
-opt.listchars = 'tab:→ ,trail:·,extends:>,precedes:<'
-opt.inccommand = 'split'
-opt.splitright = true
-opt.splitbelow = true
-opt.cursorline = true
-opt.showmode = false
-
-opt.shiftwidth = 4
-opt.tabstop = 4
+require 'dwaris.lazy'
