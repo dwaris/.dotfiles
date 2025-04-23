ec60467e9639269dbe95bafd1e7f242daf1ac3c8
dwaris <malina@rhrk.uni-kl.de>
2025-04-23 16:09:01 +0200

    nixos: sync via syncthing

--- nixos/modules/gui/backup.nix
+++ nixos/modules/gui/backup.nix
@@ -7,6 +7,15 @@
 {
   environment.systemPackages = with pkgs; [
     vorta
-    nextcloud-client
+    syncthingtray
   ];
+
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = true;
+    user = "dwaris";
+    group = "users";
+    dataDir = "/home/dwaris";  # default location for new folders
+    configDir = "/home/dwaris/.config/syncthing";
+  };
 }
