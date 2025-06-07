cac2d3ffe4e3f6b305fe6362a3da0141877e1cb1
dwaris <dwaris@itclowd.de>
2025-06-07 17:37:02 +0200

    nixos: enable smartd service

--- nixos/hosts/kashyyyk/configuration.nix
+++ nixos/hosts/kashyyyk/configuration.nix
@@ -74,6 +74,14 @@
     "adbusers"
   ];
 
+  services.zfs.autoSnapshot.enable = true;
+  services.zfs.autoScrub.enable = true;
+
+  services.smartd = {
+    autodetect = true;
+    enable = true;
+  };
+
   security.pki.certificates = [ "/etc/ssl/certs/root_ca.crt" ];
 
   hardware.bluetooth.enable = true;
