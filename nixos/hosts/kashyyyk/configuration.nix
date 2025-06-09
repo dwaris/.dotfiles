97b4343f35def9030895a3025d0962dc5979c542
dwaris <malina@rhrk.uni-kl.de>
2025-06-10 00:42:23 +0200

    nixos: move common options to module

--- nixos/hosts/kashyyyk/configuration.nix
+++ nixos/hosts/kashyyyk/configuration.nix
@@ -15,32 +15,8 @@
     ./hardware-configuration.nix
   ];
 
-  boot.loader.systemd-boot = {
-    enable = false;
-    configurationLimit = 10;
-  };
-  boot.lanzaboote = {
-    enable = true;
-    pkiBundle = "/var/lib/sbctl";
-  };
-  boot.loader.efi.efiSysMountPoint = "/boot";
-  boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages;
-  boot.supportedFilesystems = [
-    "zfs"
-    "ntfs"
-  ];
-
-  boot.zfs.requestEncryptionCredentials = true;
-  boot.zfs.forceImportRoot = true;
-
-  boot.initrd.systemd.enable = true;
-  boot.initrd.supportedFilesystems = [ "zfs" ];
-
-  boot.initrd.kernelModules = [ "zfs" ];
-  boot.kernelParams =
-    [
-    ];
+  boot.kernelParams = [ ];
 
   networking.hostName = "kashyyyk"; # Define your hostname.
   networking.hostId = "f0cacf30";
@@ -48,40 +24,10 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    sbctl
   ];
-
-  services.zfs.autoSnapshot.enable = true;
-  services.zfs.autoScrub.enable = true;
-
-  services.smartd = {
-    autodetect = true;
-    enable = true;
-  };
-
-  security.pki.certificates = [ "/etc/ssl/certs/root_ca.crt" ];
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-  systemd.services.NetworkManager-wait-online.enable = false;
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = false;
-  services.flatpak.enable = true;
-
-  environment.sessionVariables.XDG_DATA_DIRS = [ "/var/lib/flatpak/exports/share" ];
 
   services.printing.enable = false;
-  services.fwupd.enable = true;
-
   virtualisation.docker.enable = false;
-
-  programs.adb.enable = true;
-  users.users.dwaris.extraGroups = [
-    "networkmanager"
-    "docker"
-    "adbusers"
-  ];
 
   hardware.bluetooth.enable = true;
 
