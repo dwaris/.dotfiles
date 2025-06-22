ae511b71aeba640fffc1d8902afb1336d4738a0f
dwaris <malina@rhrk.uni-kl.de>
2025-06-22 22:55:30 +0200

    nixos: move desktop configuration to own module

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -4,63 +4,7 @@
   imports = [
     ./system.nix
     ./fonts.nix
+    ./desktop.nix
     ./hyprland.nix
-  ];
-
-  boot.loader.systemd-boot = {
-    enable = false;
-    configurationLimit = 10;
-  };
-  boot.lanzaboote = {
-    enable = true;
-    pkiBundle = "/var/lib/sbctl";
-  };
-  boot.loader.efi.efiSysMountPoint = "/boot";
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.supportedFilesystems = [
-    "zfs"
-    "nfs"
-    "ntfs"
-  ];
-
-  boot.zfs.requestEncryptionCredentials = true;
-  boot.zfs.forceImportRoot = true;
-
-  boot.initrd.systemd.enable = true;
-  boot.initrd.supportedFilesystems = ["zfs"];
-
-  environment.systemPackages = with pkgs; [
-    sbctl
-    nfs-utils
-  ];
-
-  services.zfs.autoSnapshot.enable = true;
-  services.zfs.autoScrub.enable = true;
-
-  services.smartd = {
-    autodetect = true;
-    enable = true;
-  };
-
-  security.pki.certificates = ["/etc/ssl/certs/root_ca.crt"];
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-  systemd.services.NetworkManager-wait-online.enable = false;
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  services.tailscale.enable = true;
-  services.flatpak.enable = true;
-
-  environment.sessionVariables.XDG_DATA_DIRS = ["/var/lib/flatpak/exports/share"];
-
-  services.fwupd.enable = true;
-
-  programs.adb.enable = true;
-  users.users.dwaris.extraGroups = [
-    "networkmanager"
-    "docker"
-    "adbusers"
   ];
 }
