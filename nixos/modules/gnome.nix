2ea9fa3d08c7ea5584913792037c21b9f26e8eea
dwaris <dwaris@itclowd.de>
2025-07-25 10:25:58 +0200

    nixos: gnonm: add some extensions

--- nixos/modules/gnome.nix
+++ nixos/modules/gnome.nix
@@ -7,6 +7,8 @@
 
   environment.systemPackages = with pkgs; [
     gnomeExtensions.appindicator
+    gnomeExtensions.space-bar
+    gnomeExtensions.tophat
     gnome-tweaks
     wl-clipboard
     adw-gtk3
