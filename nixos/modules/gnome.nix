ceb5ec228eed2a51ab44c16d106824ce72290e4c
dwaris <dwaris@itclowd.de>
2025-07-29 15:28:54 +0200

    nixos: remove unused packages

--- nixos/modules/gnome.nix
+++ nixos/modules/gnome.nix
@@ -7,8 +7,6 @@
 
   environment.systemPackages = with pkgs; [
     gnomeExtensions.appindicator
-    gnomeExtensions.space-bar
-    gnomeExtensions.tophat
     gnome-tweaks
     wl-clipboard
     adw-gtk3
