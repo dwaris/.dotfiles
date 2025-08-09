7d34d703dcb7181e9a387a4c17d54a8530da6f42
dwaris <dwaris@itclowd.de>
2025-08-10 00:06:09 +0200

    nvim: refactor lualine colors

--- nvim/.config/nvim/lua/dwaris/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/dwaris/plugins/colorscheme.lua
@@ -2,6 +2,7 @@ return {
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000,
+    auto_integrations = true,
     config = function()
         vim.cmd.colorscheme 'catppuccin'
     end,
--- nvim/.config/nvim/lua/dwaris/plugins/lualine.lua
+++ nvim/.config/nvim/lua/dwaris/plugins/lualine.lua
@@ -1,18 +1,3 @@
-local colors = {
-    bg = '#1e1e2e',
-    fg = '#CDD6F4',
-    yellow = '#ECBE7B',
-    cyan = '#94e2d5',
-    darkblue = '#081633',
-    green = '#98be65',
-    orange = '#FF8800',
-    salmon = '#f9e2af"',
-    violet = '#a9a1e1',
-    magenta = '#c678dd',
-    blue = '#51afef',
-    red = '#ec5f67',
-}
-
 local conditions = {
     buffer_not_empty = function()
         return vim.fn.empty(vim.fn.expand '%:t') ~= 1
@@ -33,14 +18,6 @@ local config = {
         -- Disable sections and component separators
         component_separators = '',
         section_separators = '',
-        theme = {
-            normal = {},
-            inactive = {},
-
-            insert = { a = { fg = colors.bg, bg = colors.cyan } },
-            visual = { a = { fg = colors.bg, bg = colors.yellow } },
-            replace = { a = { fg = colors.bg, bg = colors.red } },
-        },
     },
     sections = {
         lualine_a = {
@@ -74,10 +51,10 @@ end
 ins_left {
     'filename',
     cond = conditions.buffer_not_empty,
-    color = { fg = colors.magenta, gui = 'bold' },
+    color = { gui = 'bold' },
 }
 
-ins_left { 'progress', color = { fg = colors.fg, gui = 'bold' } }
+ins_left { 'progress', color = { gui = 'bold' } }
 
 ins_left { 'location' }
 
@@ -85,25 +62,17 @@ ins_left {
     'diagnostics',
     sources = { 'nvim_diagnostic' },
     diagnostics_color = {
-        color_error = { fg = colors.red },
-        color_warn = { fg = colors.yellow },
-        color_info = { fg = colors.cyan },
     },
 }
 
 ins_left {
     'branch',
-    color = { fg = colors.violet, gui = 'bold' },
+    color = { gui = 'bold' },
 }
 
 ins_left {
     'diff',
     symbols = { added = '+', modified = '~', removed = '-' },
-    diff_color = {
-        added = { fg = colors.green },
-        modified = { fg = colors.orange },
-        removed = { fg = colors.red },
-    },
     cond = conditions.hide_in_width,
 }
 
@@ -124,21 +93,21 @@ ins_right {
         end
         return msg
     end,
-    color = { fg = '#E4E4E4', gui = 'bold' },
+    color = { gui = 'bold' },
 }
 
 ins_right {
     'o:encoding', -- option component same as &encoding in viml
     fmt = string.upper, -- I'm not sure why it's upper case either ;)
     cond = conditions.hide_in_width,
-    color = { fg = colors.salmon, gui = 'bold' },
+    color = { gui = 'bold' },
 }
 
 ins_right {
     'fileformat',
     fmt = string.upper,
     icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
-    color = { fg = colors.salmon, gui = 'bold' },
+    color = { gui = 'bold' },
 }
 
 return {
