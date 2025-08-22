2e905d9cb92b76cfd406423e5cca02022a20c37e
dwaris <dwaris@itclowd.de>
2025-08-22 23:44:16 +0200

    nvim: fix auto init of lsps

--- nvim/.config/nvim/lua/dwaris/plugins/lsp.lua
+++ nvim/.config/nvim/lua/dwaris/plugins/lsp.lua
@@ -197,14 +197,9 @@ return {
                 },
             }
 
+            local capabilities = require('blink.cmp').get_lsp_capabilities()
+
             local servers = {}
-
-            ---@type MasonLspconfigSettings
-            ---@diagnostic disable-next-line: missing-fields
-            require('mason-lspconfig').setup {
-                automatic_enable = vim.tbl_keys(servers or {}),
-            }
-
             local ensure_installed = vim.tbl_keys(servers or {})
             vim.list_extend(ensure_installed, {
                 'stylua', -- Used to format Lua code
@@ -212,9 +207,26 @@ return {
             require('mason-tool-installer').setup {
                 ensure_installed = ensure_installed,
             }
-            for server_name, config in pairs(servers) do
-                vim.lsp.config(server_name, config)
-            end
+
+            require('mason-lspconfig').setup {
+                ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+                automatic_installation = false,
+                handlers = {
+                    function(server_name)
+                        local server = servers[server_name] or {}
+                        -- This handles overriding only values explicitly passed
+                        -- by the server configuration above. Useful when disabling
+                        -- certain features of an LSP (for example, turning off formatting for ts_ls)
+                        server.capabilities = vim.tbl_deep_extend(
+                            'force',
+                            {},
+                            capabilities,
+                            server.capabilities or {}
+                        )
+                        require('lspconfig')[server_name].setup(server)
+                    end,
+                },
+            }
         end,
     },
 
