493a46ffe9dfdde1b3590b069f7aecc76563dd82
dwaris <dwaris@itclowd.de>
2025-08-27 21:53:32 +0200

    nixos: kashyyyk: simpler battery management

--- nixos/hosts/kashyyyk/configuration.nix
+++ nixos/hosts/kashyyyk/configuration.nix
@@ -45,17 +45,7 @@
   powerManagement.powertop.enable = true;
   services = {
     thermald.enable = true;
-    power-profiles-daemon.enable = false;
-    tlp = {
-      enable = true;
-      settings = {
-        CPU_BOOST_ON_AC = 1;
-        CPU_BOOST_ON_BAT = 0;
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-        STOP_CHARGE_THRESH_BAT0 = 95;
-      };
-    };
+    power-profiles-daemon.enable = true;
   };
 
   system.stateVersion = "23.11"; # Did you read the comment?
