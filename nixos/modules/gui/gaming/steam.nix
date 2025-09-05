a659c6125bbb5f73b2cb8657c63319ac66cf66b6
dwaris <dwaris@itclowd.de>
2025-09-05 23:58:51 +0200

    nixos: bettergamemode configuration

--- nixos/modules/gui/gaming/steam.nix
+++ nixos/modules/gui/gaming/steam.nix
@@ -11,12 +11,36 @@
     "fs.file-max" = 524288;
   };
 
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
-    protontricks.enable = true;
+  programs = { 
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
+      protontricks.enable = true;
+    };
+    gamemode = {
+      enable = true;
+      settings = 
+        {
+          general = {
+            renice = 10;
+          };
+          # Warning: GPU optimisations have the potential to damage hardware
+          gpu = {
+            apply_gpu_optimisations = "accept-responsibility";
+            gpu_device = 1;
+            amd_performance_level = "high";
+          };
+          custom = {
+            start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+            end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+          };
+        };
+    };
+    # gamescope = { completly broken, capSysNice doesn't work (permission error), -e (steam) doesn't work either
+    #   enable = true;
+    #   capSysNice = true;
+    # };
   };
-  programs.gamemode.enable = true;
 }
 
