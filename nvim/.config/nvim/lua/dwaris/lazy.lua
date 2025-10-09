4817be22d4008389193f90d7b97a50c41833284b
dwaris <dwaris@itclowd.de>
2025-10-09 22:12:33 +0200

    nvim: remove lazy update notice

--- nvim/.config/nvim/lua/dwaris/lazy.lua
+++ nvim/.config/nvim/lua/dwaris/lazy.lua
@@ -1,20 +1,19 @@
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-    vim.fn.system({
+    vim.fn.system {
         'git',
         'clone',
         '--filter=blob:none',
         '--branch=stable',
         lazyrepo,
         lazypath,
-    })
+    }
 end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup {
     spec = 'dwaris.plugins',
     change_detection = { notify = false },
-    checker = { enabled = true },
     rocks = { enabled = false },
 }
