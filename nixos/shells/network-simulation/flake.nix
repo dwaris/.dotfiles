937e20df0b6650114b4c94453068780b029630e8
dwaris <dwaris@itclowd.de>
2025-10-19 17:07:00 +0200

    nixos:  network simulation development environment

---
+++ nixos/shells/network-simulation/flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              (python3.withPackages (python-pkgs: [
+                python-pkgs.networkx
+                python-pkgs.simpy
+                python-pkgs.matplotlib
+                python-pkgs.numpy
+              ]))
+            ];
+          };
+      }
+    );
+}
