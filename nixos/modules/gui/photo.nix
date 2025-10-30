3916c118fd79d9b0a6ff14e10fccc46d4ea7457c
dwaris <dwaris@itclowd.de>
2025-10-30 22:44:01 +0100

    nixos: move darktable to its own module

---
+++ nixos/modules/gui/photo.nix
@@ -0,0 +1,16 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  environment.systemPackages = with pkgs; [
+    darktable
+  ];
+  programs.gphoto2.enable = true;
+  users.users.dwaris.extraGroups = [
+    "camera"
+  ];
+}
+
