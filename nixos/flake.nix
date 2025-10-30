0bd7a53f4b7206ba0f3e1742181c2bc06f3bd8a5
dwaris <dwaris@itclowd.de>
2025-10-30 23:55:26 +0100

    nixos: fix: add rust-overlay for lanzaboote

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -14,9 +14,15 @@
 
     lanzaboote = {
       url = "github:nix-community/lanzaboote";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        rust-overlay.follows = "rust-overlay";
+      };
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     nixos-wsl.url = "github:nix-community/nixos-wsl";
 
   };
