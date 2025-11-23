e88a164ea0dc9750c32b5699f549974761fa98ec
dwaris <dwaris@itclowd.de>
2025-11-23 10:50:11 +0100

    scripts: remove day and night switch scripts for color palette

--- scripts/.scripts/switch_day.sh
+++
@@ -1,2 +0,0 @@
-sed -i 's/^palette = .*/palette = "catppuccin_latte"/' ~/.config/starship/starship.toml
-sed -i "s/\(vim.cmd.colorscheme \)'catppuccin'/\1'catppuccin-latte'/" ~/.config/nvim/lua/dwaris/plugins/theme.lua
--- scripts/.scripts/switch_night.sh
+++
@@ -1,3 +0,0 @@
-sed -i 's/^palette = .*/palette = "catppuccin_mocha"/' ~/.config/starship/starship.toml 
-sed -i "s/\(vim.cmd.colorscheme \)'catppuccin-latte'/\1'catppuccin'/" ~/.config/nvim/lua/dwaris/plugins/theme.lua
-
