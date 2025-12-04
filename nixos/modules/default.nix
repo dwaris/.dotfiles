802b8c9d018657540b01abe97aad2aff286b0d38
dwaris <dwaris@itclowd.de>
2025-12-05 00:30:26 +0100

    nixos: use specialisation for different Desktop Environments

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -1,10 +1,44 @@
-{ config, pkgs, ... }: 
+{ config, pkgs, ... }:
 
-{  
+{
   imports = [
     ./system.nix
-    ./fonts.nix
-    ./desktop.nix
-    ./hyprland.nix
+    ./boot.nix
   ];
+
+  specialisation = {
+    gnome = {
+      inheritParentConfig = true;
+      configuration = {
+        imports = [
+          ./desktop.nix
+          ./gnome.nix
+        ];
+        boot.lanzaboote.sortKey = "02";
+        system.nixos.tags = [ "gnome" ];
+      };
+    };
+    kde = {
+      inheritParentConfig = true;
+      configuration = {
+        imports = [
+          ./desktop.nix
+          ./kde.nix
+        ];
+        boot.lanzaboote.sortKey = "03";
+        system.nixos.tags = [ "kde" ];
+      };
+    };
+    hyprland = {
+      inheritParentConfig = true;
+      configuration = {
+        imports = [
+          ./desktop.nix
+          ./hyprland.nix
+        ];
+        boot.lanzaboote.sortKey = "01";
+        system.nixos.tags = [ "hyprland" ];
+      };
+    };
+  };
 }
