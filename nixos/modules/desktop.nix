f768e6c6e09e00958ad858881ff976d55787f0a1
dwaris <dwaris@itclowd.de>
2025-12-05 00:24:22 +0100

    nixos: allow boot into tty

--- nixos/modules/desktop.nix
+++ nixos/modules/desktop.nix
@@ -1,86 +1,6 @@
-{ config, pkgs, lib, ... }: 
+{ config, pkgs, ... }:
 
-let
-  zfsCompatibleKernelPackages = lib.filterAttrs (
-    name: kernelPackages:
-    (builtins.match "linux_[0-9]+_[0-9]+" name) != null
-    && (builtins.tryEval kernelPackages).success
-    && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
-  ) pkgs.linuxKernel.packages;
-  latestKernelPackage = lib.last (
-    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
-      builtins.attrValues zfsCompatibleKernelPackages
-    )
-  );
-in
-
-{
-  # Note this might jump back and forth as kernels are added or removed.
-  boot.kernelPackages = latestKernelPackage;
-
-  boot.loader.systemd-boot = {
-    enable = false;
-    configurationLimit = 10;
-  };
-  boot.lanzaboote = {
-    enable = true;
-    pkiBundle = "/var/lib/sbctl";
-  };
-  boot.loader.efi.efiSysMountPoint = "/boot";
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.supportedFilesystems = [
-    "zfs"
-    "nfs"
-    "ntfs"
-  ];
-
-  boot.zfs.requestEncryptionCredentials = true;
-  boot.zfs.forceImportRoot = true;
-
-  boot.initrd.systemd.enable = true;
-  boot.initrd.supportedFilesystems = ["zfs"];
-
-  environment.systemPackages = with pkgs; [
-    sbctl
-    nfs-utils
-    appimage-run
-  ];
-
-  services.zfs.autoSnapshot.enable = true;
-  services.zfs.autoScrub.enable = true;
-
-  services.smartd = {
-    autodetect = true;
-    enable = true;
-  };
-
-  security.pki.certificateFiles = [ ../.certs/root_ca.crt ];
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-  networking.networkmanager.plugins = with pkgs; [ networkmanager-openvpn ]; 
-  systemd.services.NetworkManager-wait-online.enable = false;
-  services.resolved.enable = true;
-
-  # Enable nftables
-  networking.firewall = {
-    enable = true;
-    trustedInterfaces = [ "virbr0" ];
-  };
-  networking.nftables.enable = true;
-
-  # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = false;
-    allowSFTP = true; 
-    openFirewall = true;
-    settings = {
-      PasswordAuthentication = false;
-      PermitRootLogin = "no";
-      AllowUsers = [ "dwaris" ];
-    };
-  };
-
+{ 
   services.printing.enable = false;
   services.avahi = {
     enable = false;
@@ -106,12 +26,4 @@ in
     pkgs.libxcrypt-legacy
     ];
   };
-
-  services.fwupd.enable = true;
-
-  programs.adb.enable = true;
-  users.users.dwaris.extraGroups = [
-    "networkmanager"
-    "adbusers"
-  ];
 }
