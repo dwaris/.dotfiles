6168ed54e3d4984e9185d03eda2a710d2d7eefb4
dwaris <dwaris@itclowd.de>
2025-12-07 17:48:04 +0100

    nixos: use gnome-keyring for specialisations

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports = [
@@ -25,8 +25,16 @@
           ./desktop.nix
           ./kde.nix
         ];
+
         boot.lanzaboote.sortKey = "03";
         system.nixos.tags = [ "kde" ];
+
+        # keep kwallet in kde module but disable it here to avoid conflicts between different desktop environments; gnome-keyring is preferred
+        services.gnome.gnome-keyring.enable = true;
+        security.pam.services.login.kwallet.enable = lib.mkForce false;
+        security.pam.services.kde.kwallet.enable = lib.mkForce false;
+        programs.ssh.startAgent = lib.mkForce false;
+        environment.sessionVariables.SSH_ASKPASS_REQUIRE = lib.mkForce "";
       };
     };
     hyprland = {
