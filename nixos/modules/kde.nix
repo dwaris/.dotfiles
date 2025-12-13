582da7f3ff07073f2b6d8cf09d078ab04b24327d
dwaris <dwaris@itclowd.de>
2025-12-13 23:02:10 +0100

    nixos: kde: remove unused exclusions and ssh agent

--- nixos/modules/kde.nix
+++ nixos/modules/kde.nix
@@ -27,14 +27,6 @@
       winDecStyles = [ "classic" ];
     })
   ];
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    plasma-browser-integration
-    konsole
-    oxygen
-    krunner
-    kwallet
-    kwalletmanager
-  ];
   
   services = {
     desktopManager.plasma6.enable = true;
@@ -44,9 +36,6 @@
       autoNumlock = true;
     };
   };
-
-  programs.ssh.startAgent = true;
-  environment.sessionVariables.SSH_ASKPASS_REQUIRE = "prefer";
 
   programs.partition-manager.enable = true;
   programs.kdeconnect.enable = false;
