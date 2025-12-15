7ff550cf8429d68fdf436a2a01b2e6d566c2e59b
dwaris <dwaris@itclowd.de>
2025-12-15 23:50:14 +0100

    nixos: kde: use gnome-keyring for browsers and vscode (how does anybody unlock kwallet in hyprland?!?!)

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -7,17 +7,17 @@
   ];
 
   specialisation = {
-    gnome = {
-      inheritParentConfig = true;
-      configuration = {
-        imports = [
-          ./desktop.nix
-          ./gnome.nix
-        ];
-        boot.lanzaboote.sortKey = "02";
-        system.nixos.tags = [ "gnome" ];
-      };
-    };
+    # gnome = {
+    #   inheritParentConfig = true;
+    #   configuration = {
+    #     imports = [
+    #       ./desktop.nix
+    #       ./gnome.nix
+    #     ];
+    #     boot.lanzaboote.sortKey = "02";
+    #     system.nixos.tags = [ "gnome" ];
+    #   };
+    # };
     kde = {
       inheritParentConfig = true;
       configuration = {
@@ -26,16 +26,12 @@
           ./kde.nix
         ];
 
-        boot.lanzaboote.sortKey = "03";
+        boot.lanzaboote.sortKey = "01";
         system.nixos.tags = [ "kde" ];
 
         # keep kwallet in kde module but disable it here to avoid conflicts between different desktop environments; gnome-keyring is preferred
         services.gnome.gnome-keyring.enable = true;
-        security.pam.services.login.kwallet.enable = lib.mkForce false;
-        security.pam.services.kde.kwallet.enable = lib.mkForce false;
-        programs.ssh.startAgent = lib.mkForce false;
-        environment.sessionVariables.SSH_ASKPASS_REQUIRE = lib.mkForce "";
-      };
+     };
     };
     hyprland = {
       inheritParentConfig = true;
@@ -44,7 +40,7 @@
           ./desktop.nix
           ./hyprland.nix
         ];
-        boot.lanzaboote.sortKey = "01";
+        boot.lanzaboote.sortKey = "02";
         system.nixos.tags = [ "hyprland" ];
       };
     };
