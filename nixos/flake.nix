90f95b36cc4397cf21425a1d75651d4d1b3f783a
dwaris <dwaris@itclowd.de>
2025-12-23 13:54:00 +0100

    Revert "nixos: add nixos-hardware"
    This reverts commit d71971592965a2caeeb13753a983305369475736.

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -11,9 +11,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
-
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-
     nix-flatpak.url = "github:gmodena/nix-flatpak/";
 
     lanzaboote = {
@@ -32,7 +29,6 @@
       self,
       nixpkgs,
       nixpkgs-stable,
-      nixos-hardware,
       lanzaboote,
       nix-flatpak,
       nixos-wsl,
@@ -60,7 +56,6 @@
           };
           modules = [
             ./hosts/kashyyyk/configuration.nix
-            nixos-hardware.nixosModules.lenovo-thinkpad-x260
             nix-flatpak.nixosModules.nix-flatpak
           ];
         };
