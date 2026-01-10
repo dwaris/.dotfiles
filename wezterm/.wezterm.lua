2e5b9d72384bd6612bb1a72951a9d1865c8c326e
dwaris <dwaris@itclowd.de>
2026-01-11 00:26:35 +0100

    nixos: update wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,64 +1,38 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-
--- This will hold the configuration.
+local act = wezterm.action
 local config = wezterm.config_builder()
 
-config.hide_tab_bar_if_only_one_tab = false
-config.use_fancy_tab_bar = false
+config.disable_default_key_bindings = true
 
 config.color_scheme = "Catppuccin Mocha"
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
 
-config.font = wezterm.font("Sauce Code Pro Nerd Font")
+config.scrollback_lines = 100000
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+config.window_close_confirmation = "NeverPrompt"
+
 config.font = wezterm.font_with_fallback({
+	"Sauce Code Pro Nerd Font",
 	"Source Code Pro",
 })
 
-config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.leader = { key = "f", mods = "CTRL" }
 
 config.unix_domains = {
 	{
 		name = "unix",
 	},
 }
-
 config.default_gui_startup_args = { "connect", "unix" }
 
-local function is_vim(pane)
-	-- this is set by the plugin, and unset on ExitPre in Neovim
-	return pane:get_user_vars().IS_NVIM == "true"
-end
-
-local direction_keys = {
-	h = "Left",
-	j = "Down",
-	k = "Up",
-	l = "Right",
-}
-
-local function split_nav(resize_or_move, key)
-	return {
-		key = key,
-		mods = resize_or_move == "resize" and "META" or "CTRL",
-		action = wezterm.action_callback(function(win, pane)
-			if is_vim(pane) then
-				-- pass the keys through to vim/nvim
-				win:perform_action({
-					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
-				}, pane)
-			else
-				if resize_or_move == "resize" then
-					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
-				else
-					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-				end
-			end
-		end),
-	}
-end
-
 config.keys = {
-	-- splitting
+	{ key = "c", mods = "CTRL|SHIFT", action = act.CopyTo("Clipboard") },
+	{ key = "v", mods = "CTRL|SHIFT", action = act.PasteFrom("Clipboard") },
+	{ key = "+", mods = "CTRL|SHIFT", action = act.IncreaseFontSize },
+	{ key = "-", mods = "CTRL|SHIFT", action = act.DecreaseFontSize },
+	{ key = "0", mods = "CTRL|SHIFT", action = act.ResetFontSize },
+
 	{
 		mods = "LEADER",
 		key = "-",
@@ -70,16 +44,15 @@ config.keys = {
 		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 	},
 	{
+		key = "q",
+		mods = "LEADER",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+	{
 		mods = "LEADER",
 		key = "m",
 		action = wezterm.action.TogglePaneZoomState,
 	},
-	{
-		mods = "LEADER",
-		key = "Space",
-		action = wezterm.action.RotatePanes("Clockwise"),
-	},
-	-- show the pane selection mode, but have it swap the active and selected panes
 	{
 		mods = "LEADER",
 		key = "0",
@@ -88,20 +61,39 @@ config.keys = {
 		}),
 	},
 	{
+		mods = "LEADER",
+		key = "c",
+		action = wezterm.action.SpawnTab("DefaultDomain"),
+	},
+	{
+		mods = "LEADER",
+		key = "n",
+		action = act.ActivateTabRelative(1),
+	},
+	{
+		mods = "LEADER",
+		key = "p",
+		action = act.ActivateTabRelative(-1),
+	},
+
+	{ mods = "LEADER", key = "h", action = act.ActivatePaneDirection("Left") },
+	{ mods = "LEADER", key = "l", action = act.ActivatePaneDirection("Right") },
+	{ mods = "LEADER", key = "k", action = act.ActivatePaneDirection("Up") },
+	{ mods = "LEADER", key = "j", action = act.ActivatePaneDirection("Down") },
+
+	{
 		key = "Enter",
 		mods = "LEADER",
 		action = wezterm.action.ActivateCopyMode,
 	},
-	-- move between split panes
-	split_nav("move", "h"),
-	split_nav("move", "j"),
-	split_nav("move", "k"),
-	split_nav("move", "l"),
-	-- resize panes
-	split_nav("resize", "LeftArrow"),
-	split_nav("resize", "DownArrow"),
-	split_nav("resize", "UpArrow"),
-	split_nav("resize", "RightArrow"),
 }
+
+for i = 1, 8 do
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = act.ActivateTab(i - 1),
+	})
+end
 
 return config
