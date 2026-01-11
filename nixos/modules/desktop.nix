5433f60e52f296473540841a00ce72432aab39bf
dwaris <dwaris@itclowd.de>
2026-01-11 18:54:19 +0100

    nixos: use tuned instead of ppd for power management

--- nixos/modules/desktop.nix
+++ nixos/modules/desktop.nix
@@ -23,6 +23,11 @@
     enable = true;
   };
 
+  services.tuned = {
+    enable = true;
+    settings.dynamic_tuning = true;
+  };
+
   services.printing = {
     enable = true;
     drivers = with pkgs; [
