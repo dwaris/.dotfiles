b8a78716c287fe15097738c7950c3549524be0cc
dwaris <dwaris@itclowd.de>
2026-01-18 10:55:03 +0100

    nixos: remove legacy mountpoint of zfs volumes

--- nixos/hosts/jedha/hardware-configuration.nix
+++ nixos/hosts/jedha/hardware-configuration.nix
@@ -44,7 +44,7 @@
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/7E8B-C715";
     fsType = "vfat";
-    options = ["fmask=0022" "dmask=0022"];
+    options = ["fmask=0077" "dmask=0077"];
   };
 
   fileSystems."/home/dwaris/Games" = {
