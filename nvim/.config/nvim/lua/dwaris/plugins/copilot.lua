f273164ef9eb7878eb9d0ad0248183608fc81401
dwaris <dwaris@itclowd.de>
2026-01-23 17:49:45 +0100

    nvim: re-enable copilot

--- nvim/.config/nvim/lua/dwaris/plugins/copilot.lua
+++ nvim/.config/nvim/lua/dwaris/plugins/copilot.lua
@@ -1,26 +1,28 @@
--- return {
---     {
---         'zbirenbaum/copilot.lua',
---         cmd = 'Copilot',
---         event = 'InsertEnter',
---         config = function()
---             require('copilot').setup {
---                 suggestion = {
---                     enabled = true,
---                     auto_trigger = false,
---                     hide_during_completion = false,
---                     debounce = 25,
---                     keymap = {
---                         accept = false,
---                         accept_word = false,
---                         accept_line = '<C-v>',
---                         next = false,
---                         prev = false,
---                         dismiss = false,
---                     },
---                 },
---             }
---         end,
---     },
--- }
-return {}
+return {
+    {
+        'zbirenbaum/copilot.lua',
+        requires = {
+            'copilotlsp-nvim/copilot-lsp', -- (optional) for NES functionality
+        },
+        cmd = 'Copilot',
+        event = 'InsertEnter',
+        config = function()
+            require('copilot').setup {
+                suggestion = {
+                    enabled = true,
+                    auto_trigger = true,
+                    hide_during_completion = false,
+                    debounce = 25,
+                    keymap = {
+                        accept = false,
+                        accept_word = false,
+                        accept_line = '<C-v>',
+                        next = false,
+                        prev = false,
+                        dismiss = false,
+                    },
+                },
+            }
+        end,
+    },
+}
