212fd32543c9eb0ddb5e9fdc3aceb1bddf526f0a
dwaris <dwaris@itclowd.de>
2026-01-26 17:07:46 +0100

    nixos: full kde

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -5,47 +5,49 @@
   ...
 }: {
   imports = [
-    ./system.nix
     ./boot.nix
+    ./system.nix
+    ./desktop.nix
+    ./kde.nix
   ];
 
-  specialisation = {
-    # gnome = {
-    #   inheritParentConfig = true;
-    #   configuration = {
-    #     imports = [
-    #       ./desktop.nix
-    #       ./gnome.nix
-    #     ];
-    #     boot.lanzaboote.sortKey = "02";
-    #     system.nixos.tags = [ "gnome" ];
-    #   };
-    # };
-    kde = {
-      inheritParentConfig = true;
-      configuration = {
-        imports = [
-          ./desktop.nix
-          ./kde.nix
-        ];
+  # specialisation = {
+  #   gnome = {
+  #     inheritParentConfig = true;
+  #     configuration = {
+  #       imports = [
+  #         ./desktop.nix
+  #         ./gnome.nix
+  #       ];
+  #       boot.lanzaboote.sortKey = "02";
+  #       system.nixos.tags = [ "gnome" ];
+  #     };
+  #   };
+  #   kde = {
+  #     inheritParentConfig = true;
+  #     configuration = {
+  #       imports = [
+  #         ./desktop.nix
+  #         ./kde.nix
+  #       ];
 
-        boot.lanzaboote.sortKey = "02";
-        system.nixos.tags = ["kde"];
+  #       boot.lanzaboote.sortKey = "02";
+  #       system.nixos.tags = ["kde"];
 
-        # keep kwallet in kde module but disable it here to avoid conflicts between different desktop environments; gnome-keyring is preferred
-        services.gnome.gnome-keyring.enable = true;
-      };
-    };
-    hyprland = {
-      inheritParentConfig = true;
-      configuration = {
-        imports = [
-          ./desktop.nix
-          ./hyprland.nix
-        ];
-        boot.lanzaboote.sortKey = "01";
-        system.nixos.tags = ["hyprland"];
-      };
-    };
-  };
+  #       # keep kwallet in kde module but disable it here to avoid conflicts between different desktop environments; gnome-keyring is preferred
+  #       services.gnome.gnome-keyring.enable = true;
+  #     };
+  #   };
+  #   hyprland = {
+  #     inheritParentConfig = true;
+  #     configuration = {
+  #       imports = [
+  #         ./desktop.nix
+  #         ./hyprland.nix
+  #       ];
+  #       boot.lanzaboote.sortKey = "01";
+  #       system.nixos.tags = ["hyprland"];
+  #     };
+  #   };
+  # };
 }
