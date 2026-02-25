61b4469cd44de082b5c45528147ba240cce3bf0b
dwaris <dwaris@itclowd.de>
2026-02-25 18:56:15 +0100

    nixos: specialisation: new paths for DEs

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -19,23 +19,16 @@
   #   gnome = {
   #     inheritParentConfig = true;
   #     configuration = {
-  #       imports = [
-  #         ./desktop.nix
-  #         ./gnome.nix
-  #       ];
-  #       boot.lanzaboote.sortKey = "02";
+  #       imports = [ ./desktop/gnome.nix ];
+  #       boot.lanzaboote.sortKey = "01";
   #       system.nixos.tags = [ "gnome" ];
   #     };
   #   };
   #   kde = {
   #     inheritParentConfig = true;
   #     configuration = {
-  #       imports = [
-  #         ./desktop.nix
-  #         ./kde.nix
-  #       ];
-
-  #       boot.lanzaboote.sortKey = "02";
+  #       imports = [ ./desktop/kde.nix ];
+  #       boot.lanzaboote.sortKey = "01";
   #       system.nixos.tags = ["kde"];
 
   #       # keep kwallet in kde module but disable it here to avoid conflicts between different desktop environments; gnome-keyring is preferred
@@ -45,11 +38,8 @@
   #   hyprland = {
   #     inheritParentConfig = true;
   #     configuration = {
-  #       imports = [
-  #         ./desktop.nix
-  #         ./hyprland.nix
-  #       ];
-  #       boot.lanzaboote.sortKey = "01";
+  #       imports = [ ./desktop/hyprland.nix ];
+  #       boot.lanzaboote.sortKey = "02";
   #       system.nixos.tags = ["hyprland"];
   #     };
   #   };
