4042e180c7b1965f572a409c274d20fe5dbb6224
dwaris <dwaris@itclowd.de>
2026-02-28 12:23:55 +0100

    nixos: enable specialisation with full kwallet support

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -8,39 +8,35 @@
     ./system.nix
 
     ./desktop
-    ./desktop/kde.nix
 
     ./cli
     ./gui
  ];
 
-  # specialisation = {
-  #   gnome = {
-  #     inheritParentConfig = true;
-  #     configuration = {
-  #       imports = [ ./desktop/gnome.nix ];
-  #       boot.lanzaboote.sortKey = "01";
-  #       system.nixos.tags = [ "gnome" ];
-  #     };
-  #   };
-  #   kde = {
-  #     inheritParentConfig = true;
-  #     configuration = {
-  #       imports = [ ./desktop/kde.nix ];
-  #       boot.lanzaboote.sortKey = "01";
-  #       system.nixos.tags = ["kde"];
-
-  #       # keep kwallet in kde module but disable it here to avoid conflicts between different desktop environments; gnome-keyring is preferred
-  #       services.gnome.gnome-keyring.enable = true;
-  #     };
-  #   };
-  #   hyprland = {
-  #     inheritParentConfig = true;
-  #     configuration = {
-  #       imports = [ ./desktop/hyprland.nix ];
-  #       boot.lanzaboote.sortKey = "02";
-  #       system.nixos.tags = ["hyprland"];
-  #     };
-  #   };
-  # };
+ specialisation = {
+    # gnome = {
+    #   inheritParentConfig = true;
+    #   configuration = {
+    #     imports = [ ./desktop/gnome.nix ];
+    #     boot.lanzaboote.sortKey = "01";
+    #     system.nixos.tags = [ "gnome" ];
+    #   };
+    # };
+    kde = {
+      inheritParentConfig = true;
+      configuration = {
+        imports = [ ./desktop/kde.nix ];
+        boot.lanzaboote.sortKey = "02";
+        system.nixos.tags = ["kde"];
+      };
+    };
+    hyprland = {
+      inheritParentConfig = true;
+      configuration = {
+        imports = [ ./desktop/hyprland.nix ];
+        boot.lanzaboote.sortKey = "01";
+        system.nixos.tags = ["hyprland"];
+      };
+    };
+  };
 }
