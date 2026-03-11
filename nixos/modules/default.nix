f651c1c5789accef68a73c963d04ace648168d57
dwaris <dwaris@itclowd.de>
2026-03-11 11:59:04 +0100

    nixos: move to kde

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -8,35 +8,36 @@
     ./system.nix
 
     ./desktop
+    ./desktop/kde.nix
 
     ./cli
     ./gui
  ];
 
- specialisation = {
-    # gnome = {
-    #   inheritParentConfig = true;
-    #   configuration = {
-    #     imports = [ ./desktop/gnome.nix ];
-    #     boot.lanzaboote.sortKey = "01";
-    #     system.nixos.tags = [ "gnome" ];
-    #   };
-    # };
-    kde = {
-      inheritParentConfig = true;
-      configuration = {
-        imports = [ ./desktop/kde.nix ];
-        boot.lanzaboote.sortKey = "02";
-        system.nixos.tags = ["kde"];
-      };
-    };
-    hyprland = {
-      inheritParentConfig = true;
-      configuration = {
-        imports = [ ./desktop/hyprland.nix ];
-        boot.lanzaboote.sortKey = "01";
-        system.nixos.tags = ["hyprland"];
-      };
-    };
-  };
+#  specialisation = {
+  # gnome = {
+  #   inheritParentConfig = true;
+  #   configuration = {
+  #     imports = [ ./desktop/gnome.nix ];
+  #     boot.lanzaboote.sortKey = "01";
+  #     system.nixos.tags = [ "gnome" ];
+  #   };
+  # };
+  #   kde = {
+  #     inheritParentConfig = true;
+  #     configuration = {
+  #       imports = [ ./desktop/kde.nix ];
+  #       boot.lanzaboote.sortKey = "02";
+  #       system.nixos.tags = ["kde"];
+  #     };
+  #   };
+  #   hyprland = {
+  #     inheritParentConfig = true;
+  #     configuration = {
+  #       imports = [ ./desktop/hyprland.nix ];
+  #       boot.lanzaboote.sortKey = "01";
+  #       system.nixos.tags = ["hyprland"];
+  #     };
+  #   };
+  # };
 }
