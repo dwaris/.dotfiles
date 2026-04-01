0ac0060f580d87b62676a53fda311d67c031c91f
dwaris <dwaris@itclowd.de>
2026-04-01 18:09:43 +0200

    nvim: remove copilot

--- nvim/.config/nvim/lua/dwaris/plugins/copilot.lua
+++ nvim/.config/nvim/lua/dwaris/plugins/copilot.lua
@@ -1,28 +1,28 @@
 return {
-    {
-        'zbirenbaum/copilot.lua',
-        requires = {
-            'copilotlsp-nvim/copilot-lsp', -- (optional) for NES functionality
-        },
-        cmd = 'Copilot',
-        event = 'InsertEnter',
-        config = function()
-            require('copilot').setup {
-                suggestion = {
-                    enabled = true,
-                    auto_trigger = true,
-                    hide_during_completion = false,
-                    debounce = 25,
-                    keymap = {
-                        accept = false,
-                        accept_word = false,
-                        accept_line = '<C-v>',
-                        next = false,
-                        prev = false,
-                        dismiss = false,
-                    },
-                },
-            }
-        end,
-    },
+    -- {
+    --     'zbirenbaum/copilot.lua',
+    --     requires = {
+    --         'copilotlsp-nvim/copilot-lsp', -- (optional) for NES functionality
+    --     },
+    --     cmd = 'Copilot',
+    --     event = 'InsertEnter',
+    --     config = function()
+    --         require('copilot').setup {
+    --             suggestion = {
+    --                 enabled = true,
+    --                 auto_trigger = true,
+    --                 hide_during_completion = false,
+    --                 debounce = 25,
+    --                 keymap = {
+    --                     accept = false,
+    --                     accept_word = false,
+    --                     accept_line = '<C-v>',
+    --                     next = false,
+    --                     prev = false,
+    --                     dismiss = false,
+    --                 },
+    --             },
+    --         }
+    --     end,
+    -- },
 }
