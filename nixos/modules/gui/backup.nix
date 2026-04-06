9da52a7ad4d35141001662d2e402a1f3dd56e950
dwaris <dwaris@itclowd.de>
2026-04-06 19:22:28 +0200

    nixos: add localsend

--- nixos/modules/gui/backup.nix
+++ nixos/modules/gui/backup.nix
@@ -8,11 +8,18 @@
     nextcloud-client
 
     vorta
+
+    localsend
   ];
 
   fileSystems."/home/dwaris/Nextcloud" = {
     device = "zpool/nextcloud";
     fsType = "zfs";
     options = ["zfsutil" "nofail"];
+  };
+
+  networking.firewall = {
+    allowedTCPPorts = [ 53317 ];
+    allowedUDPPorts = [ 53317];
   };
 }
