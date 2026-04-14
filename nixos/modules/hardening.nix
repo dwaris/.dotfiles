a2d929981342edd61da8cc8d0f4da21577ec3a92
dwaris <dwaris@itclowd.de>
2026-04-14 23:57:16 +0200

    nixos: enhance hardening configuration with additional kernel modules and security settings

--- nixos/modules/hardening.nix
+++ nixos/modules/hardening.nix
@@ -3,8 +3,37 @@
   pkgs,
   lib,
   ...
-}:{
-  boot.blacklistedKernelModules = [ "dccp" "sctp" "rds" "tipc" ];
+}: {
+  boot.blacklistedKernelModules = [
+    "dccp"
+    "sctp"
+    "rds"
+    "tipc"
+    "ax25"
+    "netrom"
+    "rose"
+    "adfs"
+    "affs"
+    "bfs"
+    "befs"
+    "cramfs"
+    "efs"
+    "erofs"
+    "exofs"
+    "freevxfs"
+    "f2fs"
+    "hfs"
+    "hpfs"
+    "jfs"
+    "minix"
+    "nilfs2"
+    "ntfs"
+    "omfs"
+    "qnx4"
+    "qnx6"
+    "sysv"
+    "ufs"
+  ];
   boot.tmp.useTmpfs = true;
 
   security.auditd.enable = true;
@@ -12,13 +41,22 @@
   services.clamav.daemon.enable = true;
   services.clamav.updater.enable = true;
 
+  boot.kernelParams = [
+    "slab_nomerge"
+    "page_poison=1"
+    "page_alloc.shuffle=1"
+    "debugfs=off"
+  ];
+
   boot.kernel.sysctl = {
     "kernel.kptr_restrict" = 1;
     "kernel.sysrq" = 16;
+    "kernel.ftrace_enabled" = 0;
 
     "net.ipv4.conf.all.rp_filter" = 2;
     "net.ipv4.conf.default.rp_filter" = 2;
 
+    "net.ipv4.icmp_echo_ignore_broadcasts" = 1;
     "net.ipv4.conf.all.accept_redirects" = 0;
     "net.ipv4.conf.default.accept_redirects" = 0;
     "net.ipv4.conf.all.secure_redirects" = 0;
@@ -34,8 +72,8 @@
   };
 
   services.openssh = {
-   settings = {
-     LogLevel = "VERBOSE";
+    settings = {
+      LogLevel = "VERBOSE";
       MaxAuthTries = 3;
       MaxSessions = 2;
       TCPKeepAlive = "no";
@@ -44,6 +82,7 @@
       AllowAgentForwarding = "no";
     };
   };
+  nix.settings.allowed-users = ["@users"];
 
-
+  security.apparmor.enable = true;
 }
