91830787d2e1b36172ce304f408009baa8bc32d4
dwaris <dwaris@itclowd.de>
2026-04-22 15:06:53 +0200

    waybar: bringup proton-vpn-cli

--- nixos/modules/gui/privacy.nix
+++ nixos/modules/gui/privacy.nix
@@ -7,6 +7,7 @@
   environment.systemPackages = with pkgs; [
     bitwarden-desktop
     proton-vpn
+    proton-vpn-cli
     qbittorrent
   ];
   environment.sessionVariables.SSH_AUTH_SOCK = "$HOME/.bitwarden-ssh-agent.sock";
