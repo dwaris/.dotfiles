d5271a1ecc780826dab881d6ed47ddb5247016f8
dwaris <dwaris@itclowd.de>
2026-04-22 11:16:07 +0200

    nvim: replace harpoon and oil with telescope

--- nvim/.config/nvim/lua/dwaris/plugins/qol.lua
+++ nvim/.config/nvim/lua/dwaris/plugins/qol.lua
@@ -26,24 +26,4 @@ return {
             },
         },
     },
-    {
-        'stevearc/oil.nvim',
-        lazy = false,
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        opts = {
-            default_file_explorer = true,
-            view_options = {
-                show_hidden = true,
-            },
-        },
-        keys = {
-            {
-                '<leader>e',
-                function()
-                    require('oil').toggle_float()
-                end,
-                desc = 'Open Oil',
-            },
-        },
-    },
 }
--- nvim/.config/nvim/lua/dwaris/plugins/telescope.lua
+++ nvim/.config/nvim/lua/dwaris/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons',
+    'nvim-telescope/telescope-file-browser.nvim',
   },
   config = function()
     local telescope = require 'telescope'
@@ -18,16 +19,16 @@ return {
     }
 
     pcall(telescope.load_extension, 'fzf')
+    pcall(telescope.load_extension, 'file_browser')
 
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
     vim.keymap.set('n', '<leader>/', function()
@@ -35,7 +36,17 @@ return {
         winblend = 10,
         previewer = false,
       })
-    end, { desc = '[/] Fuzzily search in current buffer' })
+    end, { desc = '[/] Search inside current buffer' })
+
+    vim.keymap.set('n', '<leader>e', function()
+      telescope.extensions.file_browser.file_browser {
+        path = vim.fn.expand '%:p:h',
+        cwd = vim.fn.expand '%:p:h',
+        hidden = true,
+        grouped = true,
+        select_buffer = true,
+      }
+    end, { desc = '[E]xplore files browser' })
 
     vim.keymap.set('n', '<leader>s/', function()
       builtin.live_grep {
@@ -46,6 +57,6 @@ return {
 
     vim.keymap.set('n', '<leader>sn', function()
       builtin.find_files { cwd = vim.fn.stdpath 'config' }
-    end, { desc = '[S]earch [N]eovim files' })
+    end, { desc = '[S]earch [N]eovim config files' })
   end,
 }
