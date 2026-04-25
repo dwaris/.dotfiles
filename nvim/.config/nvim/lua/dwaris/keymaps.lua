9ca4ab64cd237828125444865abf540afd2a5fa3
dwaris <dwaris@itclowd.de>
2026-04-26 00:20:04 +0200

    nvim: add diagnostic keymaps

--- nvim/.config/nvim/lua/dwaris/keymaps.lua
+++ nvim/.config/nvim/lua/dwaris/keymaps.lua
@@ -1,3 +1,7 @@
 vim.g.mapleader = ' '
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
