bcbd1eab6d03416cfafd8915e0e0c62a5274cb58
dwaris <dwaris@itclowd.de>
2026-05-22 23:17:51 +0200

    nixos: nix dev shell

---
+++ nixos/shells/nix/flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = {nixpkgs, ...}: let
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+        system: function nixpkgs.legacyPackages.${system}
+      );
+  in {
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+            nix
+            nixd
+            alejandra
+        ];
+      };
+    });
+  };
+}
