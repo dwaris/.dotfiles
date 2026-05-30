abc27303e9d83541939bf142190ea32af15bec69
dwaris <dwaris@itclowd.de>
2026-05-30 15:16:51 +0000

    nixos: minimize printer config

--- nixos/modules/desktop/default.nix
+++ nixos/modules/desktop/default.nix
@@ -32,13 +32,7 @@
     enable = true;
     drivers = with pkgs; [
       cups-filters
-      cups-browsed
     ];
-  };
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
   };
 
   security.rtkit.enable = true;
