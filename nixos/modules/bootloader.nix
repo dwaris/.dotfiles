9e06602b0bcf3edb5ff6c9362f5ad8883665ecc4
dwaris <dwaris@itclowd.de>
2026-05-31 10:43:44 +0200

    nixos: enable Plymouth

--- nixos/modules/bootloader.nix
+++ nixos/modules/bootloader.nix
@@ -1,4 +1,10 @@
 {pkgs, ...}: {
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+  ]; 
+  boot.plymouth.enable = true;
+
   boot.loader.systemd-boot = {
     enable = false;
     configurationLimit = 10;
