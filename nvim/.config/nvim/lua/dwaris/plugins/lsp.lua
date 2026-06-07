b58eda0e0a05ab36ce5967dd87a0bce79651fea2
dwaris <dwaris@itclowd.de>
2026-06-08 01:44:59 +0200

    nixos: nix: remove nixd

--- nvim/.config/nvim/lua/dwaris/plugins/lsp.lua
+++ nvim/.config/nvim/lua/dwaris/plugins/lsp.lua
@@ -178,7 +178,7 @@ return {
                         },
                     },
                 },
-                nixd = {},
+            --     nixd = {},
             --     pyright = {},
             --     ruff = {},
             --     gopls = {},
