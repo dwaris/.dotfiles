96797dfb7c49f06d5e484d7c091325b2cbd6d686
dwaris <dwaris@itclowd.de>
2026-06-08 01:17:43 +0200

    nvim: add navigation keys back and cleanup some stuff

--- nvim/.config/nvim/lua/dwaris/plugins/lsp.lua
+++ nvim/.config/nvim/lua/dwaris/plugins/lsp.lua
@@ -244,7 +244,10 @@ return {
                 toml = { 'taplo' },
             },
             notify_on_error = false,
-            format_on_save = {},
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_format = 'fallback',
+            },
             default_format_opts = {
                 timeout_ms = 500,
                 lsp_format = 'fallback',
