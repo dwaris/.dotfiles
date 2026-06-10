b7c3997419ef40d465807d42e20641cb97d73e93
dwaris <dwaris@itclowd.de>
2026-06-10 12:12:48 +0200

    nixos: add kde as second desktop

--- nixos/modules/profiles/workstation.nix
+++ nixos/modules/profiles/workstation.nix
@@ -15,39 +15,39 @@
     ];
   };
 
-  # specialisation = {
-  #   gnome = {
-  #     inheritParentConfig = true;
-  #     configuration = {
-  #       imports = [ ../desktop/gnome.nix ];
-  #       boot.lanzaboote.sortKey = "03";
-  #       system.nixos.tags = [ "gnome" ];
-  #     };
-  #   };
-  #   kde = {
-  #     inheritParentConfig = true;
-  #     configuration = {
-  #       imports = [ ../desktop/kde.nix ];
-  #       boot.lanzaboote.sortKey = "02";
-  #       system.nixos.tags = [ "kde" ];
-  #       services.gnome.gnome-keyring.enable = true;
-  #     };
-  #   };
-  #   hyprland = {
-  #     inheritParentConfig = true;
-  #     configuration = {
-  #       imports = [ ../desktop/hyprland.nix ];
-  #       boot.lanzaboote.sortKey = "01";
-  #       system.nixos.tags = [ "hyprland" ];
-  #     };
-  #   };
-  #   cosmic = {
-  #     inheritParentConfig = true;
-  #     configuration = {
-  #       imports = [ ../desktop/cosmic.nix ];
-  #       boot.lanzaboote.sortKey = "04";
-  #       system.nixos.tags = [ "cosmic" ];
-  #     };
-  #   };
-  # };
+  specialisation = {
+    # gnome = {
+    #   inheritParentConfig = true;
+    #   configuration = {
+    #     imports = [ ../desktop/gnome.nix ];
+    #     boot.lanzaboote.sortKey = "03";
+    #     system.nixos.tags = [ "gnome" ];
+    #   };
+    # };
+    kde = {
+      inheritParentConfig = true;
+      configuration = {
+        imports = [ ../desktop/kde.nix ];
+        boot.lanzaboote.sortKey = "02";
+        system.nixos.tags = [ "kde" ];
+        services.gnome.gnome-keyring.enable = true;
+      };
+    };
+    hyprland = {
+      inheritParentConfig = true;
+      configuration = {
+        imports = [ ../desktop/hyprland.nix ];
+        boot.lanzaboote.sortKey = "01";
+        system.nixos.tags = [ "hyprland" ];
+      };
+    };
+    # cosmic = {
+    #   inheritParentConfig = true;
+    #   configuration = {
+    #     imports = [ ../desktop/cosmic.nix ];
+    #     boot.lanzaboote.sortKey = "04";
+    #     system.nixos.tags = [ "cosmic" ];
+    #   };
+    # };
+  };
 }
