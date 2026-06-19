83aee33b1315a74509256c1c28c0485f1a5dec27
dwaris <dwaris@itclowd.de>
2026-06-19 22:56:54 +0200

    nixos: fix printing

--- nixos/modules/desktop/default.nix
+++ nixos/modules/desktop/default.nix
@@ -28,10 +28,17 @@
     enable = true;
   };
 
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+
   services.printing = {
     enable = true;
     drivers = with pkgs; [
       cups-filters
+      cups-browsed
     ];
   };
 
