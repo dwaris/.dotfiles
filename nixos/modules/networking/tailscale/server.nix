9275cd4bf283846758a2d5051aed807de4b2db67
dwaris <dwaris@itclowd.de>
2026-07-26 11:07:42 +0200

    nixos: move tailscale client and server into own modules

---
+++ nixos/modules/networking/tailscale/server.nix
@@ -0,0 +1,17 @@
+{pkgs, ...}: {
+  services.tailscale = {
+    enable = true;
+    useRoutingFeatures = "server";
+    openFirewall = true;
+    extraUpFlags = [
+      "--advertise-exit-node"
+      "--advertise-routes=192.168.178.0/24"
+      "--ssh"
+    ];
+  };
+
+  # Force tailscaled to use nftables instead of iptables (Critical for clean nftables-only systems)
+  systemd.services.tailscaled.serviceConfig.Environment = [
+    "TS_DEBUG_FIREWALL_MODE=nftables"
+  ];
+}
