d622e3bae5c82f7ae53ad1068a9445c5854c6f22
dwaris <dwaris@itclowd.de>
2026-07-26 11:34:25 +0200

    nixos: zfs: set new default for forceImportRoot from 26.11 on

--- nixos/modules/zfs.nix
+++ nixos/modules/zfs.nix
@@ -10,6 +10,7 @@
     "zfs"
   ];
   boot.zfs.requestEncryptionCredentials = true;
+  boot.zfs.forceImportRoot = false;
 
   boot.initrd.systemd.enable = true;
   boot.initrd.supportedFilesystems = ["zfs"];
