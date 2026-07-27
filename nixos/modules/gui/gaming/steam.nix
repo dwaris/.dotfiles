00106ca47c94e0ccb89e96f7ce2aaa1af93cc49b
dwaris <dwaris@itclowd.de>
2026-07-27 12:33:01 +0200

    nixos: enable gamescope inside of steam only

--- nixos/modules/gui/gaming/steam.nix
+++ nixos/modules/gui/gaming/steam.nix
@@ -10,6 +10,7 @@
       remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
       protontricks.enable = true;
+      gamescopeSession.enable = true;
     };
   };
   environment.systemPackages = [pkgs.pulseaudio];
