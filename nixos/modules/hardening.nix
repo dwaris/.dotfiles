86895dbe078a265f05698017beacb8dc1f2f2f6f
dwaris <dwaris@itclowd.de>
2026-08-02 11:27:10 +0200

    nixos: hardeing: don't log martians because i go blind on shutdown

--- nixos/modules/hardening.nix
+++ nixos/modules/hardening.nix
@@ -56,8 +56,6 @@
     "net.ipv4.conf.default.secure_redirects" = 0;
     "net.ipv4.conf.all.send_redirects" = 0;
     "net.ipv4.conf.default.send_redirects" = 0;
-    "net.ipv4.conf.all.log_martians" = 1;
-    "net.ipv4.conf.default.log_martians" = 1;
 
     "net.ipv4.icmp_echo_ignore_broadcasts" = 1;
     "net.ipv4.tcp_syncookies" = 1;
