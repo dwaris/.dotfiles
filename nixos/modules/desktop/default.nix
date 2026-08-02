8733abfdb767369ec330cdce39d0509e66548db6
dwaris <dwaris@itclowd.de>
2026-08-02 11:39:34 +0200

    nixos: test minimal printer setup

--- nixos/modules/desktop/default.nix
+++ nixos/modules/desktop/default.nix
@@ -25,17 +25,10 @@
     enable = true;
   };
 
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
-  };
-
   services.printing = {
     enable = true;
     drivers = with pkgs; [
       cups-filters
-      cups-browsed
     ];
   };
 
