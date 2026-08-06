6e306db1054b6c726ab5dc948106434b2b6113b3
dwaris <dwaris@itclowd.de>
2026-08-06 16:48:13 +0200

    Revert "nixos: fix hyprland not building"
    This reverts commit f559af0c47a04feeae499c2912840933ae9b7a94.

--- nixos/modules/desktop/hyprland.nix
+++ nixos/modules/desktop/hyprland.nix
@@ -99,23 +99,4 @@
   };
 
   programs.dconf.enable = true;
-
-  nixpkgs.overlays = [
-    # REVIEW: drop once
-    # https://github.com/NixOS/nixpkgs/pull/549253
-    # lands on nixos-unstable
-    (final: prev: {
-      hyprland = prev.hyprland.overrideAttrs (oldAttrs: {
-        postPatch =
-          ''
-            # Relax glaze dependency
-            # FIXME: this shouldn't be needed once the upstream code will adopt it
-            substituteInPlace CMakeLists.txt start/CMakeLists.txt hyprpm/CMakeLists.txt \
-              --replace-fail "glaze 7...<8" "glaze"
-
-          ''
-          + (oldAttrs.postPatch or "");
-      });
-    })
-  ];
 }
