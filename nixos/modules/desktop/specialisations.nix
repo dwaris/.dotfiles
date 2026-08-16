ca86f67fac480d4d6aae833dffc8da11040fc890
dwaris <dwaris@itclowd.de>
2026-08-16 11:30:47 +0200

    nixos: use specialisations

---
+++ nixos/modules/desktop/specialisations.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  ...
+}: {
+  specialisation = {
+    hyprland = {
+      inheritParentConfig = true;
+      configuration = {
+        imports = [
+          ./hyprland.nix
+        ];
+        boot.lanzaboote.sortKey = "01";
+        services.displayManager.plasma-login-manager.enable = lib.mkForce false;
+        system.nixos.tags = ["hyprland"];
+      };
+    };
+
+    kde = {
+      inheritParentConfig = true;
+      configuration = {
+        imports = [
+          ./kde.nix
+        ];
+        boot.lanzaboote.sortKey = "02";
+        services.displayManager.ly.enable = lib.mkForce false;
+        system.nixos.tags = ["kde"];
+      };
+    };
+  };
+}
