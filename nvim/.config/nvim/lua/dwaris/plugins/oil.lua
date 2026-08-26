18bb3b663dc23f3fb4a0b67eace8bc877a7237f6
dwaris <dwaris@itclowd.de>
2026-08-26 23:42:15 +0200

    nvim: close oil with q instead of C-c

--- nvim/.config/nvim/lua/dwaris/plugins/oil.lua
+++ nvim/.config/nvim/lua/dwaris/plugins/oil.lua
@@ -5,6 +5,9 @@ return {
         default_file_explorer = true,
         delete_to_trash = true,
         skip_confirm_for_simple_edits = true,
+        keymaps = {
+            ['q'] = 'actions.close',
+        },
         view_options = {
             show_hidden = true,
             natural_order = true,
