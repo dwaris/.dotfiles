ad271339fb3315de528daef0561e92454fa46c5e
dwaris <dwaris@itclowd.de>
2026-08-26 23:38:54 +0200

    nvim: use oil, remove undotree and cleanup snacks

---
+++ nvim/.config/nvim/lua/dwaris/plugins/oil.lua
@@ -0,0 +1,20 @@
+return {
+    'stevearc/oil.nvim',
+    lazy = false,
+    opts = {
+        default_file_explorer = true,
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+            show_hidden = true,
+            natural_order = true,
+            is_always_hidden = function(name, _)
+                return name == '..' or name == '.git'
+            end,
+        },
+    },
+    keys = {
+        { '-', '<cmd>Oil<cr>', desc = 'Open Parent Directory (Oil)' },
+        { '<leader>e', '<cmd>Oil<cr>', desc = 'File Explorer (Oil)' },
+    },
+}
