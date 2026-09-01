dd56df9f6112c2080b6846ef067fa8cd5aa028f4
dwaris <dwaris@itclowd.de>
2026-09-01 17:51:42 +0200

    hypr: simplify monitor config

--- hypr/.config/hypr/hypr/monitors.lua
+++ hypr/.config/hypr/hypr/monitors.lua
@@ -4,16 +4,12 @@ hl.monitor({
     mode     = "2560x1440@144",
     position = "0x0",
     scale    = 1,
-    bitdepth = 10,
-    icc      = "/home/dwaris/Nextcloud/Backup/Monitors/LG 27GP850B/rtings-LG 27GP850B.icm"
 })
 hl.monitor({
     output   = "desc:LG Electronics LG HDR 4K 0x00002991",
     mode     = "3840x2160@60.00",
     position = "auto-left",
     scale    = 1.5,
-    bitdepth = 10,
-    icc      = "/home/dwaris/Nextcloud/Backup/Monitors/LG-27UN83A/LG HDR 4K.icm",
 })
 
 -- aldhani
@@ -22,7 +18,6 @@ hl.monitor({
     mode     = "1920x1080@60",
     position = "0x0",
     scale    = 1.0,
-    icc      = "/home/dwaris/Nextcloud/Backup/Monitors/T14G2a/R140NWF5_RA.icm"
 })
 
 -- fallback
