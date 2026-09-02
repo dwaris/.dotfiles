2046b0bc928420473d54ea9a3a44403cafaa2cd0
dwaris <dwaris@itclowd.de>
2026-09-02 11:12:16 +0200

    nvim: treesitter is back

---
+++ nvim/.config/nvim/lua/dwaris/plugins/treesitter.lua
@@ -0,0 +1,39 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate',
+    config = function()
+        local ts = require 'nvim-treesitter'
+
+        ts.setup {
+            install_dir = vim.fn.stdpath 'data' .. '/site',
+        }
+
+        ts.install {
+            'bash',
+            'c',
+            'go',
+            'html',
+            'javascript',
+            'json',
+            'lua',
+            'luadoc',
+            'markdown',
+            'markdown_inline',
+            'nix',
+            'python',
+            'query',
+            'rust',
+            'typescript',
+            'vue',
+            'yaml',
+        }
+
+        -- Automatically start Treesitter highlighting on supported buffers
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function()
+                pcall(vim.treesitter.start)
+            end,
+        })
+    end,
+}
