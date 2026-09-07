465a8fd76e05929954929bb099804ac19d4c23cc
dwaris <dwaris@itclowd.de>
2026-09-07 23:17:26 +0200

    nixos: jedha: we don't boot from hdd anymore ok, time is money

--- nixos/hosts/jedha/hardware-configuration.nix
+++ nixos/hosts/jedha/hardware-configuration.nix
@@ -11,9 +11,9 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
+  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod"];
   boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-amd"];
+  boot.kernelModules = ["kvm-amd" "ahci"];
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
