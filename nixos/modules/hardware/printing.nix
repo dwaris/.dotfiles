0a933e32f78d94c5e7d82653e42c5ad55ef2d2dd
dwaris <dwaris@itclowd.de>
2026-09-07 23:47:39 +0200

    nixos: printer: keep config but service is annoying af

--- nixos/modules/hardware/printing.nix
+++ nixos/modules/hardware/printing.nix
@@ -6,23 +6,23 @@
     ];
   };
 
-  hardware.printers = {
-    ensurePrinters = [
-      {
-        name = "Brother_MFC-J5720DW";
-        location = "Arbeitszimmer";
-        description = "Brother MFC-J5720DW series";
-        deviceUri = "ipp://192.168.178.20:631/ipp/print";
-        model = "drv:///cupsfilters.drv/pwgrast.ppd";
-      }
-      {
-        name = "Brother_HL-4150CDN";
-        location = "Arbeitszimmer";
-        description = "Brother HL-4150CDN series";
-        deviceUri = "ipp://192.168.178.21:631/ipp/port1";
-        model = "drv:///sample.drv/generic.ppd";
-      }
-    ];
-    ensureDefaultPrinter = "Brother_HL-4150CDN";
-  };
+  # hardware.printers = {
+  #   ensurePrinters = [
+  #     {
+  #       name = "Brother_MFC-J5720DW";
+  #       location = "Arbeitszimmer";
+  #       description = "Brother MFC-J5720DW series";
+  #       deviceUri = "ipp://192.168.178.20:631/ipp/print";
+  #       model = "drv:///cupsfilters.drv/pwgrast.ppd";
+  #     }
+  #     {
+  #       name = "Brother_HL-4150CDN";
+  #       location = "Arbeitszimmer";
+  #       description = "Brother HL-4150CDN series";
+  #       deviceUri = "ipp://192.168.178.21:631/ipp/port1";
+  #       model = "drv:///sample.drv/generic.ppd";
+  #     }
+  #   ];
+  #   ensureDefaultPrinter = "Brother_HL-4150CDN";
+  # };
 }
