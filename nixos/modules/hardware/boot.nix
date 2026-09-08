48c156618c88067d5be8bb231fdd467f47b79cce
dwaris <dwaris@itclowd.de>
2026-09-08 20:47:02 +0200

    nixos: show boot menu

--- nixos/modules/hardware/boot.nix
+++ nixos/modules/hardware/boot.nix
@@ -1,10 +1,8 @@
 {
-  boot.loader.timeout = 0;
   boot.loader.systemd-boot = {
     enable = true;
     configurationLimit = 4;
   };
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
-
 }
