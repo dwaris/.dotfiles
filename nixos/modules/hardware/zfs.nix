58ba2f337e79f95abfa654303cfe5031f9122a24
dwaris <dwaris@itclowd.de>
2026-09-10 22:53:36 +0200

    nixos: update zfs pcr15 invalidation

--- nixos/modules/hardware/zfs.nix
+++ nixos/modules/hardware/zfs.nix
@@ -8,6 +8,7 @@
   boot.zfs.forceImportRoot = false;
 
   boot.initrd.systemd.enable = true;
+  boot.initrd.systemd.emergencyAccess = false;
   boot.initrd.supportedFilesystems = ["zfs"];
 
   boot.initrd.clevis = {
@@ -15,10 +16,19 @@
     devices."zpool".secretFile = "/etc/clevis/zpool.jwe";
   };
 
-  systemd.services.tpm-pcr15-invalidation = {
-    description = "Invalidate PCR 15 to lock disk keys away from runtime userspace";
-    wantedBy = ["multi-user.target"];
-    unitConfig.ConditionPathExists = "/dev/tpmrm0";
+  boot.initrd.systemd.services.tpm-pcr15-invalidation = {
+    description = "Invalidate PCR 15 immediately after ZFS key load";
+    requiredBy = ["sysroot.mount"];
+    wants = ["dev-tpmrm0.device"];
+    after = [
+      "zfs-import-zpool.service"
+      "dev-tpmrm0.device"
+    ];
+    before = [
+      "sysroot.mount"
+      "initrd-cleanup.service"
+    ];
+    unitConfig.AssertPathExists = "/dev/tpmrm0";
     serviceConfig = {
       Type = "oneshot";
       RemainAfterExit = true;
