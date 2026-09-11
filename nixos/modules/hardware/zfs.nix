da933d5e3aba0218e44595073a5542e4ac5a9820
dwaris <dwaris@itclowd.de>
2026-09-11 16:41:27 +0200

    nixos/zfs: remove tpm-pcr15-invalidation service for now

--- nixos/modules/hardware/zfs.nix
+++ nixos/modules/hardware/zfs.nix
@@ -16,26 +16,6 @@
     devices."zpool".secretFile = "/etc/clevis/zpool.jwe";
   };
 
-  boot.initrd.systemd.services.tpm-pcr15-invalidation = {
-    description = "Invalidate PCR 15 immediately after ZFS key load";
-    requiredBy = ["sysroot.mount"];
-    wants = ["dev-tpmrm0.device"];
-    after = [
-      "zfs-import-zpool.service"
-      "dev-tpmrm0.device"
-    ];
-    before = [
-      "sysroot.mount"
-      "initrd-cleanup.service"
-    ];
-    unitConfig.AssertPathExists = "/dev/tpmrm0";
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      ExecStart = "${pkgs.tpm2-tools}/bin/tpm2_pcrextend -T device:/dev/tpmrm0 15:sha256=0000000000000000000000000000000000000000000000000000000000000000";
-    };
-  };
-
   systemd.oomd.enable = false;
 
   services.zfs.autoSnapshot = {
