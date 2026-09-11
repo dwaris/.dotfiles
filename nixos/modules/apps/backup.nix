04804e2e9b3e2f26acf305b15eb6f041f73b3b66
dwaris <dwaris@itclowd.de>
2026-09-12 00:40:59 +0200

    nixos: vpn: add trustedInterfaces for tailscale client and server

--- nixos/modules/apps/backup.nix
+++ nixos/modules/apps/backup.nix
@@ -6,9 +6,4 @@
 
     localsend
   ];
-
-  networking.firewall.interfaces."tailscale0" = {
-    allowedTCPPorts = [53317];
-    allowedUDPPorts = [53317];
-  };
 }
--- nixos/modules/hardware/vpn/tailscale-client.nix
+++ nixos/modules/hardware/vpn/tailscale-client.nix
@@ -9,6 +9,8 @@
     ];
   };
 
+  networking.firewall.trustedInterfaces = ["tailscale0"];
+
   # Force tailscaled to use nftables instead of iptables (Critical for clean nftables-only systems)
   systemd.services.tailscaled.serviceConfig.Environment = [
     "TS_DEBUG_FIREWALL_MODE=nftables"
