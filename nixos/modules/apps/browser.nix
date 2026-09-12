9dfea3595cb63a04607c82435393b077a2cc4243
dwaris <dwaris@itclowd.de>
2026-09-12 12:03:32 +0200

    nixos: fix brave package reference in stable

--- nixos/modules/apps/browser.nix
+++ nixos/modules/apps/browser.nix
@@ -1,6 +1,11 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  brave-pkg =
+    if pkgs ? brave-origin
+    then pkgs.brave-origin
+    else pkgs.brave;
+in {
   environment.systemPackages = with pkgs; [
-    (brave-origin.override {
+    (brave-pkg.override {
       enableVideoAcceleration = true;
       commandLineArgs = [
         "--password-store=gnome-libsecret"
