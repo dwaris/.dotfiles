84287709be78d4ff386aa95e44d68fa9daea25db
dwaris <dwaris@itclowd.de>
2026-09-12 12:06:54 +0200

    nixos: update system packages

--- nixos/modules/core/system.nix
+++ nixos/modules/core/system.nix
@@ -65,16 +65,19 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    zip
-    unzip
+    _7zz
 
     curl
-    wget
     rsync
 
-    htop
     bottom
+    dust
     lm_sensors
+
+    pciutils
+    usbutils
+    lsof
+    file
   ];
 
   programs.git = {
