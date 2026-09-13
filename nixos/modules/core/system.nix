f127abca63f71029e7db2a4f8284b5d50742fd42
dwaris <dwaris@itclowd.de>
2026-09-13 18:54:10 +0200

    nixos: remove cachix

--- nixos/modules/core/system.nix
+++ nixos/modules/core/system.nix
@@ -9,13 +9,6 @@
       "nix-command"
       "flakes"
     ];
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-    ];
-
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
   };
 
   nix.optimise = {
